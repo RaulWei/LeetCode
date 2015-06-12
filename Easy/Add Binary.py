955070c17fc49adb7670a6faf89903a3b123a0ac
RaulWei <674877404@qq.com>
2015-06-12 10:20:44 +0800

    Update Add Binary.py

---
+++ Easy/Add Binary.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution:
+    # @param {string} a
+    # @param {string} b
+    # @return {string}
+    def addBinary(self, a, b):
+        p1 = a[::-1]
+        p2 = b[::-1]
+        
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.addBinary(111, 0))
