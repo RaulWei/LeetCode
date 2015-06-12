601908c34f17d25981d948f91d71d3f2313fd558
RaulWei <674877404@qq.com>
2015-06-12 22:55:35 +0800

    AC String to Integer (atoi).py
    正则表达式真神奇

--- Easy/String to Integer (atoi).py
+++ Easy/String to Integer (atoi).py
@@ -5,43 +5,26 @@ __author__ = 'Wang'
 测试用例
 前缀包括空格 正负号 0
 中间包含字母
+
+最后还是看了解题报告来做
+用了正则表达式
+没想到我越想越复杂的事情
+别人几行代码就搞定了
+不过人家用了库嘛
+一开始以为int()是不能用的
 '''
+import re
 
 class Solution:
     # @param {string} str
     # @return {integer}
     def myAtoi(self, str):
-        if not str:
-            return 0
-        s = str
-        # 确定符号
-        s = s.lstrip(' ')
-        sign = 1
-        if s[0] == '+':
-            sign = 1
-            s = s.strip('+')
-        if s[0] == '-':
-            sign = -1
-            s = s.strip('-')
-        if s[0] == '+':
-            s = s.strip('+')
-        # 删除前缀0
-        s = s.lstrip(' ')
-        s = s.lstrip('0')
-        if not s:
-            return 0
-        l_s = len(s)
-        r = 0
-        toNum = {
-            '0': 0, '1': 1,
-            '2': 2, '3': 3,
-            '4': 4, '5': 5,
-            '6': 6, '7': 7,
-            '8': 8, '9': 9,
-        }
-        for i in range(l_s):
-            r += 10**(l_s-i-1)*toNum[s[i]]*sign
-        return r
+        try:
+            str = str.strip()
+            str = re.match(r'^[+-]?\d+', str).group()
+            num = int(str)
+            return min((1<<31)-1, max(-(1<<31), num))
+        except: return 0
 
 if __name__ == '__main__':
     sol = Solution()
@@ -53,3 +36,4 @@ if __name__ == '__main__':
     print(sol.myAtoi(''))
     print(sol.myAtoi('+-2'))
     print(sol.myAtoi('-+1'))
+    print(sol.myAtoi('-3'))
