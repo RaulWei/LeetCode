05e8e487776e0d5c52c6889a50f3576121b71cee
RaulWei <674877404@qq.com>
2015-06-18 19:15:20 +0800

    AC Longest Valid Parentheses

--- Hard/Longest Valid Parentheses.py
+++ Hard/Longest Valid Parentheses.py
@@ -16,7 +16,24 @@ class Solution:
     # @param {string} s
     # @return {integer}
     def longestValidParentheses(self, s):
-        pass
+        if not s:
+            return 0
+        lens = len(s)
+        f = [0] * lens
+        for i in range(0, lens-1)[::-1]:
+            if s[i] == ')':
+                # 没有以')'开始的最长有效括号匹配
+                f[i] = 0
+            else:
+                # s[i] == '('
+                if i + f[i + 1] + 1 < lens and s[i + f[i + 1] + 1] == ')':
+                    # 跳过f[i + 1]个元素 如果下一个是')' 正好可以和'('匹配上 那么+2
+                    f[i] = f[i + 1] + 2
+                    if i + f[i + 1] + 2 < lens:
+                        # 在')'之后可能还有有效匹配 只不过一开始被')'隔开了 现在')'匹配了 自然要把后面的整合进来
+                        f[i] += f[i + f[i+1] + 2]
+        return max(f)
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.longestValidParentheses('(((()(()'))
