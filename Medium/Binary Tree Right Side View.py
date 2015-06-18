d744d01fdcea7a2a899c1d007102b0afabdf9cd0
RaulWei <674877404@qq.com>
2015-06-18 19:56:10 +0800

    AC Binary Tree Right Side View.py

--- Medium/Binary Tree Right Side View.py
+++ Medium/Binary Tree Right Side View.py
@@ -14,10 +14,31 @@ class TreeNode:
         self.right = None
 
 class Solution:
+    def rightSideOrder(self, root, res, level):
+        if not root:
+            return
+        if len(res) < level:
+            res.append(root.val)
+        self.rightSideOrder(root.right, res, level + 1)
+        self.rightSideOrder(root.left, res, level + 1)
+
     # @param {TreeNode} root
     # @return {integer[]}
     def rightSideView(self, root):
-        pass
+        res = []
+        self.rightSideOrder(root, res, 1)
+        return res
 
 if __name__ == '__main__':
+    p1 = TreeNode(1)
+    p2 = TreeNode(2)
+    p3 = TreeNode(3)
+    p4 = TreeNode(4)
+    p5 = TreeNode(5)
+    p1.left = p2
+    p1.right = p3
+    p3.right = p4
+    p2.right = p5
+
     sol = Solution()
+    print(sol.rightSideView(p1))
