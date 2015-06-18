bec7bb5aa61f10499d37559ca586b2b0c7a6e2b8
RaulWei <674877404@qq.com>
2015-06-18 14:50:22 +0800

    Update Two Sum.py

---
+++ Medium/Two Sum.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'Wang'
+
+'''
+在一个数组中找到两个数 使得这两个数的和为target
+按从小到大顺序返回这两个数的下标
+利用字典完成O(n)算法
+'''
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[]}
+    def twoSum(self, nums, target):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
