ca8ce3c5dbd61679984edf8b3fb47b778da9a8d0
RaulWei <674877404@qq.com>
2015-06-19 12:05:41 +0800

    WA Longest Common Prefix.py

--- Easy/Longest Common Prefix.py
+++ Easy/Longest Common Prefix.py
@@ -7,18 +7,20 @@ class Solution:
     def longestCommonPrefix(self, strs):
         if not strs or "" in strs:
             return ""
+        if len(strs) == 1:
+            return strs[0]
         p = 0
         while True:
             t = strs[0][p]
-            for s in strs:
-                if s[p] != t:
-                    return s[0:p]
-                if p + 1 >= len(s):
-                    return s[0:p+1]
+            for s in range(1, len(strs)):
+                if strs[s][p] != t:
+                    return strs[s][0:p]
+                if p + 1 >= len(strs[s]):
+                    return strs[s][0:p+1]
             p += 1
 
 if __name__ == '__main__':
     sol = Solution()
     #strs = ['aaa', 'aaab', 'ab']
-    strs = ["a", "b"]
+    strs = ["a", "ac"]
     print(sol.longestCommonPrefix(strs))
