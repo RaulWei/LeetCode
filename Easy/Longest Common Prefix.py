5b624ca495fab220a8d293a126908edd4f22364c
RaulWei <674877404@qq.com>
2015-06-19 14:45:25 +0800

    AC Longest Common Prefix.py

--- Easy/Longest Common Prefix.py
+++ Easy/Longest Common Prefix.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+模拟题 找出一组字符串的最长公共前缀
+'''
+
 class Solution:
     # @param {string[]} strs
     # @return {string}
@@ -11,16 +15,19 @@ class Solution:
             return strs[0]
         p = 0
         while True:
-            t = strs[0][p]
+            if p < len(strs[0]):
+                t = strs[0][p]
+            else:
+                return strs[0]
             for s in range(1, len(strs)):
+                if p >= len(strs[s]):
+                    return strs[s]
                 if strs[s][p] != t:
                     return strs[s][0:p]
-                if p + 1 >= len(strs[s]):
-                    return strs[s][0:p+1]
             p += 1
 
 if __name__ == '__main__':
     sol = Solution()
-    #strs = ['aaa', 'aaab', 'ab']
-    strs = ["a", "ac"]
-    print(sol.longestCommonPrefix(strs))
+    print(sol.longestCommonPrefix(['aaa', 'aaab', 'ab']))
+    print(sol.longestCommonPrefix(['a', 'ac']))
+    print(sol.longestCommonPrefix(['a', 'b']))
