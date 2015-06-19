ca62df8b621cc7e152f897603be7bb7375e02a22
RaulWei <674877404@qq.com>
2015-06-20 00:49:02 +0800

    WA Longest Palindromic Substring.py

--- Medium/Longest Palindromic Substring.py
+++ Medium/Longest Palindromic Substring.py
@@ -2,51 +2,39 @@
 __author__ = 'weimw'
 
 '''
-求最长回文子串
+f[i][j]表示字符串从i到j是否为回文
+初态：f[i][i] = True, 0 <= i < lens(s), f[i][j] = True, i > j 不存在的我们也认为它是
+终态：f[i][j] = True and j-i最大
+递推公式：
+if f[i+1][j-1] == True s[i] == s[j]
+    f[i][j] = True
 '''
 
 class Solution:
     # @param {string} s
     # @return {string}
     def longestPalindrome(self, s):
-        if not s:
-            return ""
         lens = len(s)
-        longestPRes = s[0]
-        i = lens / 2
-        while i >= 0 and i < lens:
-            step = 1
-            longestP = s[i]
-            while i - step >= 0 and i + step < lens:
-                if s[i - step] == s[i + step]:
-                    longestP = s[i - step] + longestP + s[i + step]
-                else:
-                    break
-                step += 1
-            if len(longestP) > len(longestPRes):
-                longestPRes = longestP
-                # 剪枝
-                if len(longestPRes) >= (len(s) - i) * 2 or len(longestPRes) >= i * 2:
-                    break
-
-            while i >= 0 and i < lens - 1:
-                step = 0
-                longestP = ''
-                while i - step >= 0 and i + 1 + step < lens:
-                    if s[i - step] == s[i + 1 + step]:
-                        longestP = s[i - step] + s[i + 1 + step]
-                    else:
-                        break
-                    step += 1
-                if len(longestP) > len(longestPRes):
-                    longestPRes = longestP
-                    if len(longestPRes) >= (len(s) - i) * 2 or len(longestPRes) >= i * 2:
-                        break
-
-            i = (i + 1) * ((-1) ** (i % 2))
-        return longestPRes
+        f = [[False for col in range(lens)] for row in range(lens)]
+        for i in range(lens):
+            for j in range(lens):
+                if i >= j:
+                    f[i][j] = True
+        for i in range(lens):
+            for j in range(lens):
+                if i+1 < lens and j-1 >= 0 and f[i+1][j-1] is True and s[i] == s[j]:
+                    f[i][j] = True
+        max, low, high = 0, 0, 0
+        for i in range(lens):
+            for j in range(lens):
+                if f[i][j] is True:
+                    if max < j - i:
+                        max = j - i
+                        low = i
+                        high = j
+        return s[low: high+1]
 
 if __name__ == '__main__':
     sol = Solution()
-    #print(sol.longestPalindrome('12345432'))
+    print(sol.longestPalindrome('34543'))
     print(sol.longestPalindrome('bb'))
