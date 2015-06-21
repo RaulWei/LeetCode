98d14d8a4bab1964c0baa24d5051616bc6e119ab
RaulWei <674877404@qq.com>
2015-06-21 11:36:50 +0800

    Update Longest Palindromic Substring-TLE.py

--- Medium/Longest Palindromic Substring-TLE.py
+++ Medium/Longest Palindromic Substring-TLE.py
@@ -8,6 +8,8 @@ f[i][j]表示字符串从i到j是否为回文
 递推公式：
 if f[i+1][j-1] == True s[i] == s[j]
     f[i][j] = True
+
+再剪枝还是O(n**2), 一直TLE
 '''
 
 class Solution:
@@ -17,17 +19,13 @@ class Solution:
         lens = len(s)
         f = [[False for col in range(lens)] for row in range(lens)]
         for i in range(lens):
-            for j in range(lens):
-                if i >= j:
-                    f[i][j] = True
+            for j in range(i + 1):
+                f[i][j] = True
+        max, low, high = 0, 0, 0
         for i in range(0, lens)[::-1]:
-            for j in range(0, lens)[::-1]:
+            for j in range(i, lens)[::-1]:
                 if i+1 < lens and j-1 >= 0 and f[i+1][j-1] is True and s[i] == s[j]:
                     f[i][j] = True
-        max, low, high = 0, 0, 0
-        for i in range(lens):
-            for j in range(lens):
-                if f[i][j] is True:
                     if max < j - i:
                         max = j - i
                         low = i
