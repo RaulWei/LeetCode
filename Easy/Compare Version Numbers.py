91616cdbd9fbbc94615b6e129277539f1e5f3900
RaulWei <674877404@qq.com>
2015-06-21 12:30:28 +0800

    WA Compare Version Numbers.py

--- Easy/Compare Version Numbers.py
+++ Easy/Compare Version Numbers.py
@@ -14,19 +14,29 @@ class Solution:
             version2 += '.0'
         ver_1 = version1.split('.')
         ver_2 = version2.split('.')
-        if int(ver_1[0]) > int(ver_2[0]):
-            return 1
-        elif int(ver_1[0]) == int(ver_2[0]):
-            if int(ver_1[1]) > int(ver_2[1]):
+        len_1 = len(ver_1)
+        len_2 = len(ver_2)
+        if len_1 < len_2:
+            for i in range(len_2 - len_1):
+                ver_1.append('0')
+        else:
+            for i in range(len_1 - len_2):
+                ver_2.append('0')
+        i = 0
+        while i < len_1 and i < len_2:
+            if int(ver_1[i]) > int(ver_2[i]):
                 return 1
-            elif int(ver_1[1]) == int(ver_2[1]):
-                return 0
-            else:
+            elif int(ver_1[i]) < int(ver_2[i]):
                 return -1
+            else:
+                i += 1
+        if len_1 == len_2:
+            return 0
+        elif len_1 > len_2:
+            return 1
         else:
             return -1
-        pass
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.compareVersion('0.1', '1.0'))
+    print(sol.compareVersion('1.1', '1.01.0'))
