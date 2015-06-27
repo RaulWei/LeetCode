ba5ba52ce6df68245d9a14b33ebc2d4bd5c8af90
RaulWei <674877404@qq.com>
2015-06-27 15:58:05 +0800

    Balanced Binary Tree

---
+++ Easy/Balanced Binary Tree.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {boolean}
+    def isBalanced(self, root):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
+
