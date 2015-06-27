93d69a16141c4815a21c0af8fe3460a6fe534ae3
RaulWei <674877404@qq.com>
2015-06-27 16:04:58 +0800

    AC

--- Easy/Balanced Binary Tree.py
+++ Easy/Balanced Binary Tree.py
@@ -9,11 +9,35 @@ class TreeNode:
         self.right = None
 
 class Solution:
+    def getDepth(self, root):
+        if not root:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        depth_left = self.getDepth(root.left)
+        depth_right = self.getDepth(root.right)
+        if depth_left < depth_right:
+            return 1 + depth_right
+        else:
+            return 1 + depth_left
+
     # @param {TreeNode} root
     # @return {boolean}
     def isBalanced(self, root):
-        pass
+        if not root:
+            return True
+        depth_left = self.getDepth(root.left)
+        depth_right = self.getDepth(root.right)
+        if abs(depth_left - depth_right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        return False
 
 if __name__ == '__main__':
     sol = Solution()
+    p1 = TreeNode(1)
+    p2 = TreeNode(1)
+    p3 = TreeNode(1)
+    p1.left = p2
+    p2.right = p3
+    print(sol.isBalanced(p1))
 
