44c14994f9454dba25f23e8f882b96f8a845887f
RaulWei <674877404@qq.com>
2015-06-28 17:17:10 +0800

    AC Linked List Cycle II.py

--- Medium/Linked List Cycle II.py
+++ Medium/Linked List Cycle II.py
@@ -1,6 +1,11 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+双指针 一快一慢
+具体推倒 https://leetcode.com/discuss/9908/o-n-solution-by-using-two-pointers-without-change-anything
+'''
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -8,13 +13,9 @@ class ListNode:
         self.next = None
 
 class Solution:
-    # @param head, a ListNode
-    # @return a list node
-    def detectCycle(self, head):
-        if head is None:
-            return None
+    # @return 如果存在环 返回快慢指针相交的位置 不存在返回None
+    def beginCycle(self, head):
         p1, p2 = head, head
-        p = None
         while p1 and p2:
             p1 = p1.next
             p2 = p2.next
@@ -22,9 +23,14 @@ class Solution:
                 return None
             p2 = p2.next
             if p1 == p2:
-                p = p1
-                break
-        p1, p2 = head, p
+                return p1
+
+    # @param head, a ListNode
+    # @return a list node
+    def detectCycle(self, head):
+        if head is None:
+            return None
+        p1, p2 = head, self.beginCycle(head)
         while p1 and p2:
             if p1 == p2:
                 return p1
