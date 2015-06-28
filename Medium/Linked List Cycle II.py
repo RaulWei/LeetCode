9f40dbaa8994c54828ec4995f9abe89843f67b48
RaulWei <674877404@qq.com>
2015-06-28 17:10:50 +0800

    AC

--- Medium/Linked List Cycle II.py
+++ Medium/Linked List Cycle II.py
@@ -14,6 +14,7 @@ class Solution:
         if head is None:
             return None
         p1, p2 = head, head
+        p = None
         while p1 and p2:
             p1 = p1.next
             p2 = p2.next
@@ -21,6 +22,20 @@ class Solution:
                 return None
             p2 = p2.next
             if p1 == p2:
+                p = p1
+                break
+        p1, p2 = head, p
+        while p1 and p2:
+            if p1 == p2:
                 return p1
+            p1 = p1.next
+            p2 = p2.next
         return None
 
+if __name__ == '__main__':
+    sol = Solution()
+    p1 = ListNode(1)
+    p2 = ListNode(2)
+    p1.next = p2
+    p2.next = p1
+    sol.detectCycle(p1)
