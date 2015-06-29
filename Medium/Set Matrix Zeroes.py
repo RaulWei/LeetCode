ba235a97407adfb3432d2c4a7764dcb187922f40
RaulWei <674877404@qq.com>
2015-06-29 20:27:59 +0800

    AC Set Matrix Zeroes.py

--- Medium/Set Matrix Zeroes.py
+++ Medium/Set Matrix Zeroes.py
@@ -1,6 +1,20 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'Wang'
 
+'''
+矩阵中遇到0 则把该0所在行和列都替换成0
+要求空间复杂度为常数 并且Do it in place
+
+我的思路是如果遇到0 则把该0所在行的第一个数变为0 所在列的第一个数变为0
+这对后序遍历没有影响 因为该行和该列的第一个数都已经遍历过
+如果要标记为0的数为矩阵左上角第一个 则特殊讨论
+
+遍历完矩阵后 开始in place地更改矩阵
+1 重新遍历矩阵的第一列 如果遇到为0 则把该行全变0
+2 重新遍历矩阵的第一行 如果遇到为0 则把该列全变0
+3 判断左上角第一个数的flag zeroRow和zeroCol 处理第一行和第一列
+'''
+
 class Solution:
     # @param {integer[][]} matrix
     # @return {void} Do not return anything, modify matrix in-place instead.
@@ -23,14 +37,17 @@ class Solution:
                     else:
                         matrix[0][c] = 0
                         matrix[r][0] = 0
+        # 遍历第一列 遇到0 则把该行全变0
         for r in range(1, row):
             if matrix[r][0] == 0:
                 for c in range(1, col):
                     matrix[r][c] = 0
+        # 遍历第一行 遇到0 则把该列全变0
         for c in range(1, col):
             if matrix[0][c] == 0:
                 for r in range(1, row):
                     matrix[r][c] = 0
+        # 特殊考虑左上角
         if zeroRow is True:
             for c in range(col):
                 matrix[0][c] = 0
