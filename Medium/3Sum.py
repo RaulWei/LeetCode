b3ae4fc0994add3abda23fe40de30bec5e46b1d3
RaulWei <674877404@qq.com>
2015-06-29 23:35:55 +0800

    AC 3Sum.py

--- Medium/3Sum.py
+++ Medium/3Sum.py
@@ -7,17 +7,18 @@ class Solution:
     def threeSum(self, nums):
         nums.sort()
         s = set()
-        dict = {}
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)):
             target = -1 * nums[i]
-            for j in range(i + 1, len(nums) - 1):
-                res = []
+            dict = {}
+            res = []
+            for j in range(i + 1, len(nums)):
                 if target - nums[j] in dict:
                     res.append(nums[i])
                     res.append(nums[j])
                     res.append(target - nums[j])
                     res.sort()
                     s.add(tuple(res))
+                    res = []
                 dict[nums[j]] = j
         ret = []
         for t in s:
@@ -26,5 +27,6 @@ class Solution:
 
 if __name__ == '__main__':
     sol = Solution()
-    # print(sol.threeSum([-1, 0, 1, 2, -1, -4]))
+    print(sol.threeSum([-1, 0, 1, 2, -1, -4]))
     print(sol.threeSum([1, 2, -2, -1]))
+    print(sol.threeSum([0, 0, 0]))
