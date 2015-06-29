11a8480eb0eef6371f753fca1f97a88d8338cf5c
RaulWei <674877404@qq.com>
2015-06-29 23:48:08 +0800

    AC 3Sum.py

--- Medium/3Sum.py
+++ Medium/3Sum.py
@@ -1,14 +1,22 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+基于two sum 做了微小的改动
+从左向右遍历 固定遍历到的数nums[i] 则target = 0 - nums[i]
+确定了target 那么从i + 1开始直到len(nums) 就完全转换为two sum这题
+'''
+
 class Solution:
     # @param {integer[]} nums
     # @return {integer[][]}
     def threeSum(self, nums):
-        nums.sort()
+        # 利用集合的去重功能
         s = set()
-        for i in range(len(nums)):
+        for i in range(len(nums) - 2):
+            # 固定一个数 求出target
             target = -1 * nums[i]
+            # 以下模仿two sum这个题 利用字典
             dict = {}
             res = []
             for j in range(i + 1, len(nums)):
@@ -18,8 +26,10 @@ class Solution:
                     res.append(target - nums[j])
                     res.sort()
                     s.add(tuple(res))
+                    # 这里找到一组合法解 重置res 不能就此跳出循环 因为这里固定nums[i] 但是可能有多组不同的合法解
                     res = []
                 dict[nums[j]] = j
+        # set to list 并返回
         ret = []
         for t in s:
             ret.append(list(t))
