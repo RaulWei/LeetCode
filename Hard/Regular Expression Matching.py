1318adb183a93dc18ae878c022fb53981f5c2f04
RaulWei <674877404@qq.com>
2015-06-30 23:44:01 +0800

    Add Regular Expression Matching.py

---
+++ Hard/Regular Expression Matching.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+'''
+实现正则匹配".*"
+参考 http://www.cnblogs.com/flowerkzj/p/3726667.html
+'''
+
+class Solution:
+    # @param {string} s
+    # @param {string} p
+    # @return {boolean}
+    def isMatch(self, s, p):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
