538d43b375120700020656ba748bd4ca2ad91ab9
RaulWei <674877404@qq.com>
2015-07-02 10:52:19 +0800

    TLE Container With Most Water.py

--- Medium/Container With Most Water.py
+++ Medium/Container With Most Water.py
@@ -19,19 +19,38 @@ class Solution:
         mleft, mright = 0, len(height) - 1
         left, right = mleft, mright
         mArea = self.getArea(height, left, right)
-        while left < right:
+        # if height[left] < height[right]:
+        while left < len(height):
             while left < len(height) - 1 and height[left] >= height[left+1]:
                 left += 1
             left += 1
-            if mArea < self.getArea(height, left, right):
-                mArea = self.getArea(height, left, right)
-            while right > 0 and height[right] >= height[right - 1]:
+            right = mright
+            while right >= 0:
+                if mArea < self.getArea(height, left, right):
+                    mArea = self.getArea(height, left, right)
+                    mleft = left
+                    mright = right
+                while right > 0 and height[right] >= height[right - 1]:
+                    right -= 1
                 right -= 1
-            right -= 1
-            if mArea < self.getArea(height, left, right):
-                mArea = self.getArea(height, left, right)
+                if left >= right:
+                    break
+
         return mArea
+            # while left < right:
+            #     while left < len(height) - 1 and height[left] >= height[left+1]:
+            #         left += 1
+            #     left += 1
+            #     if mArea < self.getArea(height, left, right):
+            #         mArea = self.getArea(height, left, right)
+            #     while right > 0 and height[right] >= height[right - 1]:
+            #         right -= 1
+            #     right -= 1
+            #     if mArea < self.getArea(height, left, right):
+            #         mArea = self.getArea(height, left, right)
+            # return mArea
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.maxArea([1, 1]))
+    # print(sol.maxArea([1, 2, 4, 3]))
+    print(sol.maxArea([2, 3, 10, 5, 7, 8, 9]))
