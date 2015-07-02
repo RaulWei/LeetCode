c898274c93bab795f08d8f0a41ed3b2b8b3f0a03
RaulWei <674877404@qq.com>
2015-07-02 17:07:49 +0800

    AC Letter Combinations of a Phone Number.py

--- Medium/Letter Combinations of a Phone Number.py
+++ Medium/Letter Combinations of a Phone Number.py
@@ -1,17 +1,41 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'Wang'
 
+'''
+模拟手机九宫格键盘 输入数字 输出可能的字母组合
+不带标记的回溯算法
+类似求全排列 比全排列简单
+'''
+
+import copy
+
 class Solution:
+    # 数字与字母对应的哈希表
     numToCh = [
         [' '], [''], ['a', 'b', 'c'], ['d', 'e', 'f'],
         ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
         ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']
     ]
+
+    def backtracking(self, step, digits, res_t, res):
+        if step == len(digits):
+            # 停止条件
+            res_t_cp = copy.deepcopy(res_t)
+            res.append(''.join(res_t_cp))
+            return
+        for i in range(len(self.numToCh[int(digits[step])])):
+            res_t[step] = self.numToCh[int(digits[step])][i]
+            self.backtracking(step + 1, digits, res_t, res)
+
     # @param {string} digits
     # @return {string[]}
     def letterCombinations(self, digits):
-        i = 0
-        return
+        if not digits:
+            return []
+        res_t = [0 for i in range(len(digits))]
+        res = []
+        self.backtracking(0, digits, res_t, res)
+        return res
 
 if __name__ == '__main__':
     sol = Solution()
