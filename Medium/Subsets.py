90576e1c5346f0d55f8d681fad27869d97538871
RaulWei <674877404@qq.com>
2015-07-02 20:48:28 +0800

    AC Subsets.py

--- Medium/Subsets.py
+++ Medium/Subsets.py
@@ -1,31 +1,33 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+求集合的所有子集
+对于集合中的每个元素 都存在两种可能：选 or 不选
+'''
+
 import copy
 
 class Solution:
-    def backtracking(self, step, nums, book, res_t, res):
+    def backtracking(self, step, nums, res_t, res):
         if step == len(nums):
             res_t_cp = copy.deepcopy(res_t)
-            res_t_cp.remove('')
             res.append(res_t_cp)
             return
-        for i in range(len(nums)):
-            if book[i] == 0:
-                res_t[step] = nums[i]
-                if nums[i] != '':
-                    book[i] = 1
-                self.backtracking(step + 1, nums, book, res_t, res)
-                book[i] = 0
+
+        # 选
+        res_t.append(nums[step])
+        self.backtracking(step + 1, nums, res_t, res)
+        # 不选
+        res_t.remove(nums[step])
+        self.backtracking(step + 1, nums, res_t, res)
 
     # @param {integer[]} nums
     # @return {integer[][]}
     def subsets(self, nums):
-        nums.append('')
-        book = [0 for i in range(len(nums))]
-        res_t = [0 for i in range(len(nums))]
-        res = []
-        self.backtracking(0, nums, book, res_t, res)
+        res_t, res = [], []
+        nums.sort()
+        self.backtracking(0, nums, res_t, res)
         return res
 
 if __name__ == '__main__':
