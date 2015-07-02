5e05547d4e40c560c32b687c2e0b1d71707783aa
RaulWei <674877404@qq.com>
2015-07-02 14:42:14 +0800

    Add Letter Combinations of a Phone Number.py

---
+++ Medium/Letter Combinations of a Phone Number.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'Wang'
+
+class Solution:
+    numToCh = [
+        [' '], [''], ['a', 'b', 'c'], ['d', 'e', 'f'],
+        ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'],
+        ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']
+    ]
+    # @param {string} digits
+    # @return {string[]}
+    def letterCombinations(self, digits):
+        i = 0
+        return
+
+if __name__ == '__main__':
+    sol = Solution()
+    sol.letterCombinations('23')
