32cbcb389af9636d190bc81107bca7898f0b868f
RaulWei <674877404@qq.com>
2015-07-07 16:50:35 +0800

    AC Kth Smallest Element in a BST.py

---
+++ Medium/Kth Smallest Element in a BST.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'Wang'
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def totalTreeNodeNum(self, root):
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        return 1 + self.totalTreeNodeNum(root.left) + self.totalTreeNodeNum(root.right)
+
+    # @param {TreeNode} root
+    # @param {integer} k
+    # @return {integer}
+    def kthSmallest(self, root, k):
+        lChildNum = self.totalTreeNodeNum(root.left)
+        if k <= lChildNum:
+            return self.kthSmallest(root.left, k)
+        if lChildNum + 1 == k:
+            return root.val
+        if k > lChildNum + 1:
+            return self.kthSmallest(root.right, k - lChildNum - 1)
+
+if __name__ == '__main__':
+    sol = Solution()
+    p1 = TreeNode(5)
+    p2 = TreeNode(3)
+    p3 = TreeNode(7)
+    p4 = TreeNode(1)
+    p5 = TreeNode(4)
+    p6 = TreeNode(6)
+    p7 = TreeNode(8)
+    p1.left = p2
+    p1.right = p3
+    p2.left = p4
+    p2.right = p5
+    p3.left = p6
+    p3.right = p7
+    print(sol.kthSmallest(p1, 6))
