b3920862a0153be7553335cf0717c378fbfb59f6
RaulWei <674877404@qq.com>
2015-07-08 14:13:53 +0800

    Update

--- Medium/Minimum Size Subarray Sum.py
+++ Medium/Minimum Size Subarray Sum.py
@@ -7,7 +7,7 @@ class Solution:
     # @return {integer}
     def minSubArrayLen(self, s, nums):
         lenth = len(nums)
-        minLen = lenth
+        minLen = lenth + 1
         b, e, sum = 0, 0, 0
         while e < lenth:
             while sum < s and e < lenth:
@@ -15,15 +15,17 @@ class Solution:
                 sum += nums[e]
                 e += 1
             if sum < s:
-                # 所有nums的和加起来都比s小
-                return 0
+                # 从b开始到最后 所有nums的和加起来都比s小
+                break
             while b < e and sum >= s:
                 # b向右 缩小sum
                 sum -= nums[b]
                 b += 1
             if minLen > e - b + 1:
                 minLen = e - b + 1
-        return minLen
+        if minLen <= lenth:
+            return minLen
+        return 0
 
 if __name__ == '__main__':
     sol = Solution()
