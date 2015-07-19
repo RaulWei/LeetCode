c03a0971e39811454d09287c0696adc88a9bcb62
RaulWei <674877404@qq.com>
2015-07-19 21:37:45 +0800

    AC Implement Queue using Stacks.py

---
+++ Easy/Implement Queue using Stacks.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Queue:
+    # initialize your data structure here.
+    def __init__(self):
+        self.q = []
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        self.q.append(x)
+
+    # @return nothing
+    def pop(self):
+        self.q = self.q[1::]
+
+    # @return an integer
+    def peek(self):
+        if not q:
+            return None
+        return self.q[0]
+
+    # @return an boolean
+    def empty(self):
+        if not self.q:
+            return True
+        return False
+
+if __name__ == '__main__':
+    q = Queue()
+    q.push(1)
+    q.push(2)
+    q.pop()
+    q.pop()
+
