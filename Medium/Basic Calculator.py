2a2803eb7d9732f5509e8dafdc80c7d3bab93bc1
RaulWei <674877404@qq.com>
2015-07-21 16:24:28 +0800

    Add Basic Calculator.py

---
+++ Medium/Basic Calculator.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'Wang'
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def calculate(self, s):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
