91eb3fddb356759bab07e22e2274271834efffee
RaulWei <674877404@qq.com>
2015-07-25 13:21:19 +0800

    Add Count Complete Tree Nodes

---
+++ Medium/Count Complete Tree Nodes.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {integer}
+    def countNodes(self, root):
+        if not root:
+            return 0
+        rLeft, rRight = root, root
+        cLeft, cRight = 1, 1
+        while rLeft:
+            rLeft =  rLeft.left
+            cLeft += 1
+        while rRight:
+            rRight = rRight.right
+            cRight += 1
+        if cLeft == cRight:
+            return 2 ** cLeft - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+if __name__ == '__main__':
+    sol = Solution()
