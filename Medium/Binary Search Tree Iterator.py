117fc9cba77d9b4158050ad0d435c73a6f11bf35
RaulWei <674877404@qq.com>
2015-07-26 13:25:22 +0800

    Add Binary Search Tree Iterator

---
+++ Medium/Binary Search Tree Iterator.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+    # @param root, a binary search tree's root node
+    def __init__(self, root):
+        self.root = root
+
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        if self.root.left:
+            return True
+
+    # @return an integer, the next smallest number
+    def next(self):
+        self.root = self.root.left
+        return self.root
+
+# Your BSTIterator will be called like this:
+# i, v = BSTIterator(root), []
+# while i.hasNext(): v.append(i.next())
