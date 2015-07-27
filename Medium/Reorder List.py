070beba1aab3ca4df864f3a0f056c04e5e6257a2
RaulWei <674877404@qq.com>
2015-07-28 00:22:21 +0800

    Update Reorder List

--- Medium/Reorder List.py
+++ Medium/Reorder List.py
@@ -11,7 +11,22 @@ class Solution:
     # @param {ListNode} head
     # @return {void} Do not return anything, modify head in-place instead.
     def reorderList(self, head):
-        pass
+        p1, p2 = head, head
+        while p2.next and p2.next.next:
+            p1 = p1.next
+            p2 = p2.next.next
+        h1, h2 = head, self.reverseList(p1.next)
+        p1.next = None
+        r, h = h1, h1
+        while h1 and h2:
+            h1 = h1.next
+            h.next = h2
+            h = h.next
+
+            h2 = h2.next
+            h.next = h1
+            h = h.next
+        return r
 
     def reverseList(self, head):
         if not head:
@@ -23,3 +38,14 @@ class Solution:
         t.next = head
         head.next = None
         return h
+
+if __name__ == '__main__':
+    sol = Solution()
+    p1 = ListNode(1)
+    p2 = ListNode(2)
+    p3 = ListNode(3)
+    p4 = ListNode(4)
+    p1.next = p2
+    p2.next = p3
+    p3.next = p4
+    sol.reorderList(p1)
