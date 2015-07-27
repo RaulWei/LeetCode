ea4e5a38c53aeb8b4f827782b4893316927d8a81
RaulWei <674877404@qq.com>
2015-07-27 21:44:04 +0800

    AC Bitwise AND of Numbers Range

--- Medium/Bitwise AND of Numbers Range.py
+++ Medium/Bitwise AND of Numbers Range.py
@@ -3,7 +3,8 @@ __author__ = 'weimw'
 
 '''
 0 <= m <= n <= 2**31
-位运算
+只要m!=n 那么从m到n的求and最低位必为0
+把m和n都向右移 直到m等于n
 '''
 
 class Solution:
@@ -11,17 +12,12 @@ class Solution:
     # @param {integer} n
     # @return {integer}
     def rangeBitwiseAnd(self, m, n):
-        res = 0
-        for i in range(0, 32)[::-1]:
-            t = 1
-            k = m
-            while k < n + 1:
-                t &= k >> i & 1
-                k += 1
-                if t == 0:
-                    break
-            res = (res << 1) | t
-        return res
+        step = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            step += 1
+        return m << step
 
 if __name__ == '__main__':
     sol = Solution()
@@ -29,3 +25,4 @@ if __name__ == '__main__':
     print(sol.rangeBitwiseAnd(54, 117))
     print(sol.rangeBitwiseAnd(0, 2147483647))
     print(sol.rangeBitwiseAnd(600000000, 2147483645))
+    print(sol.rangeBitwiseAnd(33, 45))
