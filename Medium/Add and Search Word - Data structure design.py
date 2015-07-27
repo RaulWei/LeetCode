dcb85c89d7e7ac4aea6094285664b974c593e66b
RaulWei <674877404@qq.com>
2015-07-27 11:19:59 +0800

    Add

---
+++ Medium/Add and Search Word - Data structure design.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+'''
+添加和查找单词
+字典树
+'''
+
+class WordDictionary:
+    # initialize your data structure here.
+    def __init__(self):
+
+
+    # @param {string} word
+    # @return {void}
+    # Adds a word into the data structure.
+    def addWord(self, word):
+
+
+    # @param {string} word
+    # @return {boolean}
+    # Returns if the word is in the data structure. A word could
+    # contain the dot character '.' to represent any one letter.
+    def search(self, word):
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# wordDictionary = WordDictionary()
+# wordDictionary.addWord("word")
+# wordDictionary.search("pattern")
