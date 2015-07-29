8e5a68febb694caef5a22b3f944fcdd073b0895b
RaulWei <674877404@qq.com>
2015-07-29 14:12:52 +0800

    AC Lowest Common Ancestor of a Binary Tree

--- Medium/Lowest Common Ancestor of a Binary Tree.py
+++ Medium/Lowest Common Ancestor of a Binary Tree.py
@@ -1,6 +1,11 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+求任意二叉树中两个节点的最小祖先
+递归解法
+'''
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -15,15 +20,18 @@ class Solution:
     # @return {TreeNode}
     def lowestCommonAncestor(self, root, p, q):
         if not root or root == p or root == q:
+            # 结束条件
             return root
+
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
         if left and right:
+            # 两边都不空说明一左一右
             return root
         if left and not right:
+            # p,q都在左边
             return left
         if not left and right:
+            # P,q都在右边
             return right
-
-
