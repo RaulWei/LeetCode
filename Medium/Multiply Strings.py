2a3fa3b95d2800675da526ff2eef67cf4d3f523e
RaulWei <674877404@qq.com>
2015-08-11 09:59:15 +0800

    Multiply String done

--- Medium/Multiply Strings.py
+++ Medium/Multiply Strings.py
@@ -8,15 +8,15 @@ class Solution:
     def multiply(self, num1, num2):
         lens1 = len(num1)
         lens2 = len(num2)
-        res = [0 * (lens1 + lens2)]
+        res = [0] * (lens1 + lens2)
         num1 = num1[::-1]
         num2 = num2[::-1]
         c = 0
         for i in range(lens1):
             for j in range(lens2):
-                r = int(num1[i]) * int(num2[j]) + c
+                r = int(num1[i]) * int(num2[j]) + c + res[i + j]
                 c = r / 10
-                res[i + j] += r % 10
+                res[i + j] = r % 10
             if c != 0:
                 res[i + j + 1] += c
                 c /= 10
