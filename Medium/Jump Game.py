230dd9ec62b5d33548edb11cbaea3945571f72ee
RaulWei <674877404@qq.com>
2015-08-12 18:21:45 +0800

    AC Jump Game

--- Medium/Jump Game.py
+++ Medium/Jump Game.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+主要是能否跨过0 或者 到达0而0刚好是最后一个数字
+根据0的位置往前倒推
+通过维持一个MAX变量进行剪枝 否则内重的for循环太多次
+'''
+
 class Solution:
     # @param {integer[]} nums
     # @return {boolean}
@@ -14,6 +20,7 @@ class Solution:
             if nums[i] == 0:
                 for j in range(max(i-MAX, 0), i):
                     if nums[j] > i - j or (nums[j] == i - j and i == len(nums) - 1):
+                        # 能跨过0 or 踩到0而且0是最后一位
                         flag = 1
                         break
                 if flag == 1:
