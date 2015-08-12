af2203f11919f11d1ab70e21162d8d812964f3f9
RaulWei <674877404@qq.com>
2015-08-12 21:09:07 +0800

    AC

---
+++ Medium/Group Anagrams.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution:
+    # @param {string[]} strs
+    # @return {string[][]}
+    def groupAnagrams(self, strs):
+        if not strs:
+            return []
+        dic, res = {}, []
+        for str in strs:
+            sorted_str = ''.join(sorted(str))
+            if sorted_str in dic:
+                res[dic[sorted_str]].append(str)
+            else:
+                dic[sorted_str] = len(dic)
+                res.append([str])
+        for r in res:
+            r = r.sort()
+        return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
