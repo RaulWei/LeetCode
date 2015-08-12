49a07ddeeb15e5af0097de29c525e5d825e8abba
RaulWei <674877404@qq.com>
2015-08-12 17:22:20 +0800

    AC

--- Medium/Jump Game.py
+++ Medium/Jump Game.py
@@ -5,13 +5,15 @@ class Solution:
     # @param {integer[]} nums
     # @return {boolean}
     def canJump(self, nums):
+        if len(nums) == 1:
+            return True
         MAX, flag = 0, 0
         for i in range(len(nums)):
             if MAX < nums[i]:
                 MAX = nums[i]
             if nums[i] == 0:
-                for j in range(i-MAX, i):
-                    if nums[j] > i - j:
+                for j in range(max(i-MAX, 0), i):
+                    if nums[j] > i - j or (nums[j] == i - j and i == len(nums) - 1):
                         flag = 1
                         break
                 if flag == 1:
@@ -20,3 +22,8 @@ class Solution:
                     return False
         return True
 
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.canJump([2, 3, 1, 1, 4]))
+    print(sol.canJump([3, 2, 1, 0, 4]))
+    print(sol.canJump([2, 0, 0]))
