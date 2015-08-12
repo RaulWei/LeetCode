46cb287ee153f82cc01cae944a0b3e3edc70395e
RaulWei <674877404@qq.com>
2015-08-12 11:18:40 +0800

    AC Implement strStr()

--- Easy/Implement strStr().py
+++ Easy/Implement strStr().py
@@ -7,11 +7,13 @@ class Solution:
     # @return {integer}
     def strStr(self, haystack, needle):
         if not needle:
+            # needle是0 说明是可以从haystack找到needle的
             return 0
         if not haystack or len(haystack) < len(needle):
             return -1
         p1, p2 = 0, len(needle)
         while p2 <= len(haystack):
+            # 固定窗口大小进行滑动
             if haystack[p1:p2] == needle:
                 return p1
             p1 += 1
