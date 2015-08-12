3cc869e4812ed875a0d79ab92af91004d664f938
RaulWei <674877404@qq.com>
2015-08-12 22:21:04 +0800

    AC Spiral Matrix

--- Medium/Spiral Matrix.py
+++ Medium/Spiral Matrix.py
@@ -1,6 +1,11 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+纯模拟走地图
+右 - 下 - 左 - 上
+'''
+
 class Solution:
     # @param {integer[][]} matrix
     # @return {integer[]}
@@ -11,24 +16,28 @@ class Solution:
         res = []
         i, j = 0, 0
         while len(res) < m * n:
+            # 向右
             while j < n and matrix[i][j] != '#':
                 res.append(matrix[i][j])
                 matrix[i][j] = '#'
                 j += 1
             j -= 1
             i += 1
+            # 向下
             while i < m and matrix[i][j] != '#':
                 res.append(matrix[i][j])
                 matrix[i][j] = '#'
                 i += 1
             i -= 1
             j -= 1
+            # 向左
             while j >= 0 and matrix[i][j] != '#':
                 res.append(matrix[i][j])
                 matrix[i][j] = '#'
                 j -= 1
             j += 1
             i -= 1
+            # 向上
             while i >= 0 and matrix[i][j] != '#':
                 res.append(matrix[i][j])
                 matrix[i][j] = '#'
@@ -36,3 +45,8 @@ class Solution:
             i += 1
             j += 1
         return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(sol.spiralOrder(matrix))
