c7ef10b812c46e9ed5c1d956017f0e5e7e364820
RaulWei <674877404@qq.com>
2015-08-14 16:02:23 +0800

    AC First Missing Positive

--- Hard/First Missing Positive.py
+++ Hard/First Missing Positive.py
@@ -1,12 +1,22 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+乱序数组中找第一个未出现的整数
+时间复杂度O(n) 空间复杂度O(1)
+
+归位之后希望是A[0] = 1, A[1] = 2, A[i] = i+1, ..
+'''
+
 class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def firstMissingPositive(self, nums):
         for i in range(len(nums)):
             while 0 < nums[i] <= len(nums) and nums[nums[i]-1] != nums[i]:
+                # 将nums[i]归位 len(nums)应该归位到nums[len(nums)-1] 所以这里取了等号
+                # 后一个判断 如果为 nums[i] != i+1 判断i处是否有归位数 这种判断处理[1, 1]用例死循环
+                # 后一个判断 改为nums[nums[i]-1] != nums[i] 判断nums[i]这个数是否归位 可以处理重复数不陷入死循环
                 t = nums[i]
                 nums[i] = nums[t-1]
                 nums[t-1] = t
