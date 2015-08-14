487e9d627142b91da6c32d07978808ad8ae178e5
RaulWei <674877404@qq.com>
2015-08-14 13:16:49 +0800

    AC Trapping Rain Water in one scan time

--- Hard/Trapping Rain Water.py
+++ Hard/Trapping Rain Water.py
@@ -7,4 +7,20 @@ class Solution:
     def trap(self, height):
         res, maxleft, maxright, left, right = 0, 0, 0, 0, len(height) - 1
         while left <= right:
-            
+            if height[left] < height[right]:
+                if height[left] > maxleft:
+                    maxleft = height[left]
+                else:
+                    res += maxleft - height[left]
+                left += 1
+            else:
+                if height[right] > maxright:
+                    maxright = height[right]
+                else:
+                    res += maxright - height[right]
+                right -= 1
+        return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
