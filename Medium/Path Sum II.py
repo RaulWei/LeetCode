d53998ec230c44467b0a769048610d0b160eec9a
RaulWei <674877404@qq.com>
2015-08-15 23:48:37 +0800

    add Path Sum II

---
+++ Medium/Path Sum II.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @param {integer} sum
+    # @return {integer[][]}
+    def pathSum(self, root, sum):
+        if not root:
+            return
+        print(root.val)
+        self.pathSum(root.left, sum)
+        self.pathSum(root.right, sum)
+
+if __name__ == '__main__':
+    p1 = TreeNode(5)
+    p2 = TreeNode(4)
+    p3 = TreeNode(8)
+    p4 = TreeNode(11)
+    p5 = TreeNode(13)
+    p6 = TreeNode(4)
+    p7 = TreeNode(7)
+    p8 = TreeNode(2)
+    p9 = TreeNode(5)
+    p10 = TreeNode(1)
+    p1.left = p2
+    p1.right = p3
+    p2.left = p4
+    p3.left = p5
+    p3.right = p6
+    p4.left = p7
+    p4.right = p8
+    p6.left = p9
+    p6.right = p10
+    sol = Solution()
+    sol.pathSum(p1, 22)
+
