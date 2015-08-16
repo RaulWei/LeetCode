efbc324ff80614e8d6ef67f21124fa700d53d90f
RaulWei <674877404@qq.com>
2015-08-17 00:03:04 +0800

    AC Maximum Product Subarray

--- Medium/Maximum Product Subarray.py
+++ Medium/Maximum Product Subarray.py
@@ -4,6 +4,11 @@ __author__ = 'weimw'
 '''
 m[i]表示以下标i结尾的连续最大乘积
 n[i]表示以下标i结尾的连续最小乘积
+初态：m[0] = n[0] = nums[0]
+终态：max(m[0])
+递归公式：
+nums[i] >= 0: m[i] = max(m[i-1]*nums[i], nums[i]), n[i] = min(n[i-1]*nums[i], nums[i])
+nums[i] < 0: m[i] = max(n[i-1]*nums[i], nums[i]), n[i] = min(m[i-1]*nums[i], nums[i])
 '''
 
 class Solution:
