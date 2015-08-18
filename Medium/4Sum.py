cc1640d4686e507f56f6e1e97f59ae5b2b3dfa96
RaulWei <674877404@qq.com>
2015-08-18 15:30:17 +0800

    AC 4Sum

---
+++ Medium/4Sum.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer[][]}
+    def fourSum(self, nums, target):
+        s = set()
+        for i in range(len(nums) - 3):
+            for j in range(i + 1, len(nums) - 2):
+                newTarget = target - nums[i] - nums[j]
+                dict, res = {}, []
+                for k in range(j + 1, len(nums)):
+                    if newTarget - nums[k] in dict:
+                        res.append(nums[i])
+                        res.append(nums[j])
+                        res.append(nums[k])
+                        res.append(newTarget - nums[k])
+                        res.sort()
+                        s.add(tuple(res))
+                        res = []
+                    dict[nums[k]] = k
+        ret = []
+        for t in s:
+            ret.append(list(t))
+        return ret
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.fourSum([1, 0, -1, 0, -2, 2], 0))
