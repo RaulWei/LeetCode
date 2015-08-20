ec91ae3c1d294870420405f55c99ed3c6a20bf31
RaulWei <674877404@qq.com>
2015-08-20 12:36:11 +0800

    AC Substring with Concatenation of All Words

--- Hard/Substring with Concatenation of All Words.py
+++ Hard/Substring with Concatenation of All Words.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
-import copy
+
+'''
+勉强AC
+注意words可以包含重复的word
+http://bangbingsyb.blogspot.jp/2014/11/leetcode-substring-with-concatenation.html
+以上的思路和我很像 可以参考
+'''
 
 class Solution:
     # @param {string} s
@@ -9,20 +15,20 @@ class Solution:
     def findSubstring(self, s, words):
         res, dic = [], {}
         word_num, word_len = len(words), len(words[0])
+        # 初始化 建立字典
         for i in range(word_num):
             if words[i] not in dic:
                 dic[words[i]] = 1
             else:
                 dic[words[i]] += 1
-        # i = 0
-        # while i <= len(s) - word_len * word_num:
+        # 遍历s 找到符合的索引
         for i in range(len(s) - word_len * word_num + 1):
             if self.isivalid(i, s, word_num, word_len, dic):
                 res.append(i)
-            # i += word_len
         return res
 
     def isivalid(self, i, s, word_num, word_len, dic):
+        # 本来令local_dic = copy.deepcopy(dic)然后做相应判断 这样开销太大 TLE
         k, local_dic = 0, {}
         while k < word_num:
             word = s[i + word_len * k: i + word_len * (k + 1)]
