d8d68165d9aeac9a35c3622e6d850952bd0d7de6
RaulWei <674877404@qq.com>
2015-08-20 11:16:01 +0800

    WA

--- Hard/Substring with Concatenation of All Words.py
+++ Hard/Substring with Concatenation of All Words.py
@@ -9,8 +9,11 @@ class Solution:
         res, dic = [], {}
         word_num, word_len = len(words), len(words[0])
         for i in range(word_num):
-            dic[words[i]] = 1
-        for i in range(len(s) - word_len):
+            if words[i] not in dic:
+                dic[words[i]] = 1
+            else:
+                dic[words[i]] += 1
+        for i in range(len(s) - word_len * word_num):
             if self.isivalid(i, s, word_num, word_len, dic):
                 res.append(i)
         return res
@@ -20,6 +23,9 @@ class Solution:
         while k < word_num:
             if s[i + word_len * k: i + word_len * (k + 1)] not in dic:
                 return False
+            dic[s[i + word_len * k: i + word_len * (k + 1)]] -= 1
+            if dic[s[i + word_len * k: i + word_len * (k + 1)]] == 0:
+                dic.pop(s[i + word_len * k: i + word_len * (k + 1)])
             k += 1
         return True
 
