3be9ff43ed048359850eb423f7c28be57d5eba74
RaulWei <674877404@qq.com>
2015-08-20 09:19:40 +0800

    update

---
+++ Hard/Search in Rotated Sorted Array.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution:
+    # @param {integer[]} nums
+    # @param {integer} target
+    # @return {integer}
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (low + high) / 2
+            if target == nums[mid]:
+                return mid
+            # 以下的判断中target都不等于mid
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
