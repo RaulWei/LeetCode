9b34f866fc1c7b7eb7411ba4c4d00f9221739d15
RaulWei <674877404@qq.com>
2015-08-22 16:05:40 +0800

    AC Jump Game II

--- Hard/Jump Game II.py
+++ Hard/Jump Game II.py
@@ -1,15 +1,25 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+f[i]表示从0到i需要的最少步数 初始化f[0]=0
+根据每个nums[i]更新其后的nums[i]个f[i+x]的值
+终态f[len(nums)-1]即为所求
+注意剪枝
+'''
+
 # :type nums: List[int]
 # :rtype: int
 class Solution(object):
     def jump(self, nums):
         f = [32767] * len(nums)
         f[0] = 0
+        # 循环每个nums[i] 求其后nums[i]个f的值
         for i in range(len(nums)):
+            # 剪枝条件
             if i > 0 and nums[i] < nums[i - 1]:
                 continue
+            # 判断更新nums[i]后nums[i]个f值
             for j in range(1, nums[i] + 1):
                 if i + j < len(nums) and f[i] + 1 < f[i + j]:
                     f[i + j] = f[i] + 1
