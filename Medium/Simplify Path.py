6fd86b5a84326e94d425712751d7efb45e0dbc12
RaulWei <674877404@qq.com>
2015-08-26 22:31:45 +0800

    AC Simplify Path

--- Medium/Simplify Path.py
+++ Medium/Simplify Path.py
@@ -1,6 +1,13 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+split处理path得到pathList 对每个pathList中的content
+1 等于空或者'.' 什么都不做
+2 等于'..' 弹出栈顶
+3 等于其他 入栈
+'''
+
 class Solution(object):
     # :type path: str
     # :rtype: str
@@ -14,9 +21,10 @@ class Solution(object):
                 res.pop()
             elif content != "..":
                 res.append(content)
-        return '/' + '/'.join(res)
+        return '/' + '/'.join(res)  # '/'.join(res)表示'/'夹在每个res成员中间
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.simplifyPath("/../"))
     print(sol.simplifyPath("/home/"))
     print(sol.simplifyPath("/a/./b/../../c/"))
