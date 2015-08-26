f5e95ab0b38410030f15e87231955426091241d3
Mingwei Wei <weimw0417@163.com>
2015-08-26 18:03:09 +0800

    WA

--- Medium/Missing Number.py
+++ Medium/Missing Number.py
@@ -5,11 +5,18 @@ class Solution(object):
     # :type nums: List[int]
     # :rtype: int
     def missingNumber(self, nums):
-        for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        return len(nums)
+        fact_sum, max = 0, 0
+        for n in nums:
+            fact_sum += n
+            if n > max:
+                max = n
+        ideal_sum = (max * (max + 1)) / 2
+        if ideal_sum == fact_sum:
+            return max + 1
+        return ideal_sum - fact_sum
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.missingNumber([0]))
+    print(sol.missingNumber([1]))
     print(sol.missingNumber([0, 1, 3]))
