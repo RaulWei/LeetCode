4127bd28a47c0f3c4aa95debbbd34007297f6164
RaulWei <674877404@qq.com>
2015-08-28 14:57:12 +0800

    AC Edit Distance

--- Hard/Edit Distance.py
+++ Hard/Edit Distance.py
@@ -16,15 +16,20 @@ class Solution(object):
     def minDistance(self, word1, word2):
         f = [[0 for col in range(len(word2) + 1)] for row in range(len(word1) + 1)]
         # 初始化
-        for i in range(len(word1)):
+        for i in range(len(word1) + 1):
             f[i][0] = i
-        for j in range(len(word2)):
+        for j in range(len(word2) + 1):
             f[0][j] = j
         # 动态规划
-        for i in range(1, len(word1)):
-            for j in range(1, len(word2)):
-                if word1[i] == word2[j]:
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
+                if word1[i - 1] == word2[j - 1]:
                     f[i][j] = f[i - 1][j - 1]
                 else:
                     f[i][j] = min(f[i - 1][j - 1] + 1, f[i - 1][j] + 1, f[i][j - 1] + 1)
         return f[len(word1)][len(word2)]
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.minDistance("a", "b"))
+    print(sol.minDistance("", "a"))
