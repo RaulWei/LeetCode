7f14f224dbbacc4e4297635cabc5e3d9772c0fb4
RaulWei <674877404@qq.com>
2015-08-28 14:53:42 +0800

    update Edit Distance submit first time WA

--- Hard/Edit Distance.py
+++ Hard/Edit Distance.py
@@ -2,9 +2,9 @@
 __author__ = 'weimw'
 
 '''
-f[i][j]表示word1[i]变化到word2[j]的编辑距离
+f[i][j]表示word1前i个组成的字符串变化到word2前j个组成的字符串的编辑距离
 初态：
-终态：f[len(word1) - 1][len(word2) - 1]
+终态：f[len(word1)][len(word2)]
 递推公式：
 
 '''
@@ -14,7 +14,7 @@ class Solution(object):
     # :type word2: str
     # :rtype: int
     def minDistance(self, word1, word2):
-        f = [[0 for col in range(len(word2))] for row in range(len(word1))]
+        f = [[0 for col in range(len(word2) + 1)] for row in range(len(word1) + 1)]
         # 初始化
         for i in range(len(word1)):
             f[i][0] = i
@@ -26,4 +26,5 @@ class Solution(object):
                 if word1[i] == word2[j]:
                     f[i][j] = f[i - 1][j - 1]
                 else:
-                    pass
+                    f[i][j] = min(f[i - 1][j - 1] + 1, f[i - 1][j] + 1, f[i][j - 1] + 1)
+        return f[len(word1)][len(word2)]
