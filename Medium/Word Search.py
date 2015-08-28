d2f6784a94a55d8a6285802d279b50c72063e4ab
RaulWei <674877404@qq.com>
2015-08-28 21:04:09 +0800

    WA Word Search

---
+++ Medium/Word Search.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution(object):
+    # :type board: List[List[str]]
+    # :type word: str
+    # :rtype: bool
+    def exist(self, board, word):
+        for i in range(len(board)):
+            board[i] = list(board[i][0])
+
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == word[0] and self.DFS(i, j, board, word, 1):
+                    return True
+        return False
+
+    def DFS(self, i, j, board, word, index):
+        if index == len(word):
+            return True
+        dirc = [[0, 1], [1, 0], [0, -1], [-1, 0]]   # 右 下 左 上
+        for go in dirc:
+            new_i, new_j = i + go[0], j + go[1]
+            if 0 <= new_i < len(board) and 0 <= new_j < len(board[new_i]) and \
+                            board[new_i][new_j] == word[index] and \
+                    self.DFS(new_i, new_j, board, word, index + 1):
+                return True
+        return False
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.exist([["ABCE"], ["SFCS"], ["ADEE"]], "ABCB"))
+    print(sol.exist([["ABCE"], ["SFCS"], ["ADEE"]], "ABCCED"))
+    print(sol.exist([["ABCE"], ["SFCS"], ["ADEE"]], "SEE"))
