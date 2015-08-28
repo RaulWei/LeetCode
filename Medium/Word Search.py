618d986b7df06921e3470141c414ac3db7442570
RaulWei <674877404@qq.com>
2015-08-28 21:29:07 +0800

    AC Word Search

--- Medium/Word Search.py
+++ Medium/Word Search.py
@@ -1,18 +1,17 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+本题的题目介绍的输入格式和测试用例不符 题目给的是类似[["ab"], ["cd"]] 实际上是["ab", "cd"]
+简单的深搜回溯没什么难点 也没卡时间
+'''
+
 class Solution(object):
     # :type board: List[List[str]]
     # :type word: str
     # :rtype: bool
     def exist(self, board, word):
-        # for i in range(len(board)):
-        #     if len(board) == 1:
-        #         board[i] = list(board[i])
-        #     else:
-        #         board[i] = list(board[i][0])
         walked = [[0 for col in range(len(board[0]))] for row in range(len(board))]
-
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j] == word[0]:
@@ -29,10 +28,11 @@ class Solution(object):
         for go in dirc:
             new_i, new_j = i + go[0], j + go[1]
             if 0 <= new_i < len(board) and 0 <= new_j < len(board[new_i]) and board[new_i][new_j] == word[index] and walked[new_i][new_j] == 0:
+                # 新点合法 且能匹配 且没走过
                 walked[new_i][new_j] = 1
                 if self.DFS(new_i, new_j, board, word, index + 1, walked):
                     return True
-                walked[new_i][new_j] = 0
+                walked[new_i][new_j] = 0    # 注意复原
         return False
 
 if __name__ == '__main__':
