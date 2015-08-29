9279e4dad8f835830540a9955797bc47004c17eb
RaulWei <674877404@qq.com>
2015-08-29 16:57:11 +0800

    update

--- Hard/Minimum Window Substring.py
+++ Hard/Minimum Window Substring.py
@@ -14,16 +14,36 @@ class Solution(object):
         is_found = False
 
         for ch in t:
-            t_num[int(ch)] += 1
+            t_num[ord(ch)] += 1
 
         for i in range(len(s)):
             # 先找到包含所有t字符的大窗口
             while i < len(s) and found_num < len(t):
-                if window_num[int(s[i])] < t_num[int(s[i])]:
+                if window_num[ord(s[i])] < t_num[ord(s[i])]:
                     found_num += 1
-                window_num[int(s[i])] += 1
+                window_num[ord(s[i])] += 1
                 i += 1
             # 找到大窗口后减小窗口
             if found_num == len(t):
                 is_found = True
                 min_right = i - 1
+                while left < min_right:
+                    if window_num[ord(s[left])] > t_num[ord(s[left])]:
+                        window_num[ord(s[left])] -= 1
+                        left += 1
+                    else:
+                        break
+                min_left = left
+                if min_len > min_right - min_left + 1:
+                    min_len = min_right - min_left + 1
+                    min_index = min_left
+                window_num[ord(s[left])] -= 1
+                found_num -= 1
+                left += 1
+        if is_found:
+            return s[min_index:min_len]
+        return ''
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.minWindow("ADOBECODEBANC", "ABC"))
