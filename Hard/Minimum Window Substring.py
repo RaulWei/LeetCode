03c0f4901943438e335ac012b907c5b63ebe6c8b
RaulWei <674877404@qq.com>
2015-08-29 17:52:39 +0800

    AC Minimum Window Substring

--- Hard/Minimum Window Substring.py
+++ Hard/Minimum Window Substring.py
@@ -3,6 +3,7 @@ __author__ = 'weimw'
 
 '''
 滑动窗口法 细节很难处理
+参考算法 http://m4tiku.duapp.com/report?pid=85
 '''
 
 class Solution(object):
@@ -12,17 +13,18 @@ class Solution(object):
     def minWindow(self, s, t):
         if not s or not t:
             return ''
-        left, right, min_left, min_right = 0, 0, 0, 0
+        left, min_left, min_right = 0, 0, 0
         min_len, min_index, found_num = len(s), 0, 0
         t_num, window_num = [0 for col in range(256)], [0 for col in range(256)]
         is_found = False
 
+        # 初始化t的字符统计数组
         for ch in t:
             t_num[ord(ch)] += 1
 
         i = 0
         while i < len(s):
-            # 先找到包含所有t字符的大窗口
+            # 先找到包含所有t中字符的大窗口
             while i < len(s) and found_num < len(t):
                 if window_num[ord(s[i])] < t_num[ord(s[i])]:
                     found_num += 1
@@ -33,15 +35,18 @@ class Solution(object):
                 is_found = True
                 min_right = i - 1
                 while left < min_right:
+                    # 收缩窗口左边 找min_left
                     if window_num[ord(s[left])] > t_num[ord(s[left])]:
                         window_num[ord(s[left])] -= 1
                         left += 1
                     else:
                         break
                 min_left = left
+                # 找到本轮的最小窗口 更新min_index min_len
                 if min_len > min_right - min_left + 1:
                     min_len = min_right - min_left + 1
                     min_index = min_left
+                # 找下一轮
                 window_num[ord(s[min_left])] -= 1
                 found_num -= 1
                 left = min_left + 1
