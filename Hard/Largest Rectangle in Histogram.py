e6e1e3dd14261eac72d22b2be72b225b6b068d43
RaulWei <674877404@qq.com>
2015-08-31 10:18:14 +0800

    AC Largest Rectangle in Histogram

--- Hard/Largest Rectangle in Histogram.py
+++ Hard/Largest Rectangle in Histogram.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+一开始的想法是遍历每个长方形 向左向右扩展找最大 结果超时
+实在是想不出有什么更好的办法
+参考 http://www.cnblogs.com/felixfang/p/3676193.html 构造递增
+'''
+
 class Solution(object):
     # :type height: List[int]
     # :rtype: int
@@ -11,8 +17,8 @@ class Solution(object):
         while i < len(height):
             while stack and height[i] < height[stack[-1]]:
                 tmp = stack.pop()
-                left_area = height[tmp] * (tmp + 1 if not stack else tmp - stack[-1])
-                right_area = height[tmp] * (i - tmp - 1)
+                left_area = height[tmp] * (tmp + 1 if not stack else tmp - stack[-1])   # 以tmp为高度 tmp所在柱以及向左延伸出的面积
+                right_area = height[tmp] * (i - tmp - 1)    # 以tmp为高度 向右延伸出的面积
                 area = left_area + right_area
                 max_area = area if area > max_area else max_area
             stack.append(i)
