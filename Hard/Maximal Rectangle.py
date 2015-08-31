8fcb3846f3da3ab4b632e8fc12e2d3a23ab9293b
RaulWei <674877404@qq.com>
2015-08-31 22:29:56 +0800

    AC Maximal Rectangle

--- Hard/Maximal Rectangle.py
+++ Hard/Maximal Rectangle.py
@@ -5,6 +5,10 @@ __author__ = 'weimw'
 本题算是Largest Rectangle in Histogram的进阶
 求01矩阵中包含1最多的矩形
 对于矩阵的每一行看作直方图 立柱的高度就是行元素往上数1的个数
+最后求各行结果的最大值即可
+参考 http://www.cnblogs.com/felixfang/p/3676193.html
+
+注意 本题的输入不是[["111"], ["111"]] 而是["111", "111"]
 '''
 
 class Solution(object):
@@ -14,13 +18,15 @@ class Solution(object):
         max_area = 0
         for row in range(len(matrix)):
             matrix[row] += '#'
-            left_area, right_area = 0, 0
             i, stack = 0, []
             while i < len(matrix[row]):
                 while stack and self.height(matrix, row, i) < self.height(matrix, row, stack[-1]):
                     tmp = stack.pop()
-                    left_area = self.height(matrix, row, tmp) * (tmp + 1 if not stack else tmp - stack[-1])   # 以tmp为高度 tmp所在柱以及向左延伸出的面积
-                    right_area = self.height(matrix, row, tmp) * (i - tmp - 1)    # 以tmp为高度 向右延伸出的面积
+                    tmp_height = self.height(matrix, row, tmp)
+                    # 以tmp为高度 tmp所在柱以及向左延伸出的面积
+                    left_area = tmp_height * (tmp + 1 if not stack else tmp - stack[-1])
+                    # 以tmp为高度 向右延伸出的面积
+                    right_area = tmp_height * (i - tmp - 1)
                     area = left_area + right_area
                     max_area = area if area > max_area else max_area
                 stack.append(i)
