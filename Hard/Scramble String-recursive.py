ca058258361091b482425c02ba0bbfc01ea3e155
Mingwei Wei <weimw0417@163.com>
2015-09-02 13:37:51 +0800

    AC Scramble String-recursive

--- Hard/Scramble String-recursive.py
+++ Hard/Scramble String-recursive.py
@@ -21,14 +21,14 @@ class Solution(object):
 
         # 递归
         for i in range(1, len(s1)):
-            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:len(s1)], s2[i:len(s2)])):
-                return True
-            if self.isScramble(s1[:i], s2[i:]) and self.isScramble(s1[i:], s2[:i]):
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) \
+                    or (self.isScramble(s1[:i], s2[len(s2)-i:]) and self.isScramble(s1[i:], s2[:len(s2)-i])):
                 return True
         return False
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.isScramble("abc", "bca"))
     print(sol.isScramble("abb", "bba"))
     print(sol.isScramble("great", "rgeat"))
     print(sol.isScramble("great", "rgtae"))
