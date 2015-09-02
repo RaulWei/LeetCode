1ffe804172913d60d7fa3c66cea82e35e9c8dfde
RaulWei <674877404@qq.com>
2015-09-02 18:06:17 +0800

    update

--- Medium/Restore IP Addresses.py
+++ Medium/Restore IP Addresses.py
@@ -5,3 +5,8 @@ class Solution(object):
     # :type s: str
     # :rtype: List[str]
     def restoreIpAddresses(self, s):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.restoreIpAddresses("1010815125"))
