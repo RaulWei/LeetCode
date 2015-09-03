2dd56a9ff5c181fc6ef1ec420b7f7af94af8e57a
RaulWei <674877404@qq.com>
2015-09-03 16:12:08 +0800

    update Restore IP Addresses

--- Medium/Restore IP Addresses.py
+++ Medium/Restore IP Addresses.py
@@ -7,6 +7,11 @@ class Solution(object):
     def restoreIpAddresses(self, s):
         pass
 
+    def is_valid(self, str):
+        if len(str) > 3 or len(str) == 0 or (len(str) > 1 and str[0] == '0') or int(str) > 255:
+            return False
+        return True
+    
 if __name__ == '__main__':
     sol = Solution()
     print(sol.restoreIpAddresses("1010815125"))
