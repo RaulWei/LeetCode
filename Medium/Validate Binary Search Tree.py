ea376f5285d91228b22fab34a447538a9f37e39a
RaulWei <674877404@qq.com>
2015-09-05 19:02:47 +0800

    AC

--- Medium/Validate Binary Search Tree.py
+++ Medium/Validate Binary Search Tree.py
@@ -1,6 +1,8 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+import sys
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -12,25 +14,12 @@ class Solution(object):
     # :type root: TreeNode
     # :rtype: bool
     def isValidBST(self, root):
-        return self.checkBST(root, -1, 65535)
+        return self.checkBST(root, -1 * sys.maxint - 1, sys.maxint)
 
     def checkBST(self, root, left_val, right_val):
         # root为None
         if not root:
             return True
-        # 没有左子树 也没有右子树
-        if not root.left and not root.right:
-            return True
-        # 没有左子树 有右子树
-        if not root.left:
-            if right_val > root.val and self.checkBST(root.right, root.val, right_val):
-                return True
-            return False
-        # 没有右子树 有左子树
-        if not root.right:
-            if left_val < root.val and self.checkBST(root.left, left_val, root.val):
-                return True
-            return False
         # 有左子树 有右子树
         if left_val < root.val < right_val and self.checkBST(root.left, left_val, root.val) and self.checkBST(root.right, root.val, right_val):
             return True
@@ -39,8 +28,8 @@ class Solution(object):
 if __name__ == '__main__':
     sol = Solution()
     p1 = TreeNode(1)
-    p2 = TreeNode(2)
+    p2 = TreeNode(1)
     p3 = TreeNode(3)
     p1.right = p2
-    p2.right = p3
+    # p2.right = p3
     print(sol.isValidBST(p1))
