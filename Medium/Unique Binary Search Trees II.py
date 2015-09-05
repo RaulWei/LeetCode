c7b3959ac3826fa8e317d93fa99fbcc43a22f7b1
RaulWei <674877404@qq.com>
2015-09-05 16:09:06 +0800

    AC

--- Medium/Unique Binary Search Trees II.py
+++ Medium/Unique Binary Search Trees II.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
-import copy
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, x):
@@ -13,26 +12,24 @@ class Solution(object):
     # :type n: int
     # :rtype: List[TreeNode]
     def generateTrees(self, n):
-        res = []
-        ret = self.genTrees(1, n, res)
+        ret = self.genTrees(1, n)
         return ret
 
     # 返回从begin到end为根的树的根节点集合
-    def genTrees(self, begin, end, res):
+    def genTrees(self, begin, end):
         if begin > end:
-            res.append(None)
-            return res
-
+            return [None]
+        ret = []
         for i in range(begin, end + 1):
-            left_sub_tree = copy.deepcopy(self.genTrees(begin, i - 1, res))    # 左子树根节点集合
-            right_sub_tree = copy.deepcopy(self.genTrees(i + 1, end, res))     # 右子树根节点集合
+            left_sub_tree = self.genTrees(begin, i - 1)    # 左子树根节点集合
+            right_sub_tree = self.genTrees(i + 1, end)     # 右子树根节点集合
             for left in left_sub_tree:
                 for right in right_sub_tree:
                     root = TreeNode(i)
                     root.left = left
                     root.right = right
-                    res.append(root)
-        return res
+                    ret.append(root)
+        return ret
 
 if __name__ == '__main__':
     sol = Solution()
