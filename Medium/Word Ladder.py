9a1a44c505acff0ad99d1ce78808df2740045a1c
Mingwei Wei <weimw0417@163.com>
2015-09-08 11:11:51 +0800

    add word ladder

---
+++ Medium/Word Ladder.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'wang'
+
+class Solution(object):
+    # :type beginWord: str
+    # :type endWord: str
+    # :type wordDict: Set[str]
+    # :rtype: int
+    def ladderLength(self, beginWord, endWord, wordDict):
+        pass
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]))
