0fcaee3428da8d37c331a5dcc03f4fe565b46b0e
Mingwei Wei <weimw0417@163.com>
2015-09-08 20:15:02 +0800

    TLE Word Ladder

--- Medium/Word Ladder.py
+++ Medium/Word Ladder.py
@@ -7,7 +7,25 @@ class Solution(object):
     # :type wordDict: Set[str]
     # :rtype: int
     def ladderLength(self, beginWord, endWord, wordDict):
-        pass
+        wordDict = list(wordDict)
+        toVisit, dist = [], 2
+        wordDict.append(endWord)
+        self.addNextWords(beginWord, wordDict, toVisit)
+        while toVisit:
+            num = len(toVisit)
+            for i in range(num):
+                word = toVisit.pop(0)
+                if word == endWord:
+                    return dist
+                self.addNextWords(word, wordDict, toVisit)
+            dist += 1
+        return 0
+
+    def addNextWords(self, curWord, wordDict, toVisit):
+        for w in wordDict:
+            if self.isOneChDiff(curWord, w):
+                toVisit.append(w)
+                wordDict.remove(w)
 
     def isOneChDiff(self, word1, word2):
         if len(word1) != len(word2):
@@ -22,4 +40,5 @@ class Solution(object):
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.ladderLength("hit", "cog", ["hig"]))
     print(sol.ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]))
