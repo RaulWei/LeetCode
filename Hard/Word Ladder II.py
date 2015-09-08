3d187363ad51846ab34733d84b88516f0c780dd5
Mingwei Wei <weimw0417@163.com>
2015-09-08 11:16:44 +0800

    add word ladder II

---
+++ Hard/Word Ladder II.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'wang'
+
+class Solution(object):
+    # :type start: str
+    # :type end: str
+    # :type wordlist: Set[str]
+    # :rtype: List[List[int]]
+    def findLadders(self, start, end, wordlist):
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.findLadders("hit", "cog", ["hot","dot","dog","lot","log"])
