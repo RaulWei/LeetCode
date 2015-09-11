9168175ac05968bdfdd4ec549451e4812f2a973f
Mingwei Wei <weimw0417@163.com>
2015-09-11 17:03:16 +0800

    add Surrounded Regions

---
+++ Medium/Surrounded Regions.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'wang'
+
+'''
+'''
+
+class Solution(object):
+    # :type board: List[List[str]]
+    # :rtype: void Do not return anything, modify board in-place instead.
+    def solve(self, board):
+        pass
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.solve(["XXXX", "XOOX", "XXOX", "XOXX"]))
