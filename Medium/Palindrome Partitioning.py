6b61394e349453d437fdfebfc01052940dd429ca
RaulWei <674877404@qq.com>
2015-09-13 13:12:18 +0800

    update Palindrome Partitioning

--- Medium/Palindrome Partitioning.py
+++ Medium/Palindrome Partitioning.py
@@ -1,7 +1,12 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
-
+class Solution(object):
+    # :type s: str
+    # :rtype: List[List[str]]
+    def partition(self, s):
+        pass
 
 if __name__ == '__main__':
     sol = Solution()
+    sol.partition("aab")
