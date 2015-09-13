6f85ca81d8a20a458aac1a46a17225a678563205
RaulWei <674877404@qq.com>
2015-09-13 22:32:33 +0800

    AC Palindrome Partitioning

--- Medium/Palindrome Partitioning.py
+++ Medium/Palindrome Partitioning.py
@@ -1,12 +1,42 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+import copy
+
 class Solution(object):
     # :type s: str
     # :rtype: List[List[str]]
     def partition(self, s):
-        pass
+        self.ret = []
+        res = []
+        self.DFS(s, res, 0)
+        return self.ret
+
+    def DFS(self, s, res, start):
+        if start == len(s):
+            self.ret.append(copy.deepcopy(res))
+            return
+        for i in range(start + 1, len(s) + 1):
+            if self.isPalindrome(s[start:i]):
+                res.append(s[start:i])
+                self.DFS(s, res, i)
+                res.pop()
+
+    def isPalindrome(self, s):
+        if not s:
+            return False
+        lens = len(s)
+        for i in range(lens / 2):
+            if s[i] != s[lens - i - 1]:
+                return False
+        return True
 
 if __name__ == '__main__':
     sol = Solution()
-    sol.partition("aab")
+    print(sol.isPalindrome(""))
+    print(sol.isPalindrome("1"))
+    print(sol.isPalindrome("111"))
+    print(sol.isPalindrome("dfd"))
+
+    sol.partition("a")
+    # sol.partition("aab")
