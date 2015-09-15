74f21a5563a4da37c77049e95f296b4b3150fa0a
Mingwei Wei <weimw0417@163.com>
2015-09-15 11:15:55 +0800

    AC Binary Tree Maximum Path Sum

--- Hard/Binary Tree Maximum Path Sum.py
+++ Hard/Binary Tree Maximum Path Sum.py
@@ -7,9 +7,7 @@ import sys
 maxPathDown(root)表示从root开始向下的单向path最大值
 maxPathDown(root) =
     0, root = None
-    root.val, maxPathDown(root.left) <=0 and maxPathDown(root.right) <= 0
-    maxPathDown(root.left), maxPathDown(root.left) > 0 and maxPathDown(root.left) > maxPathDown(root.right)
-    maxPathDown(root.right), maxPathDown(root.right) > 0 and maxPathDown(root.right) > maxPathDown(root.left)
+    max(max(maxPathDown(left), 0), max(maxPathDown(right), 0)) + root.val
 '''
 
 # Definition for a binary tree node.
@@ -34,7 +32,10 @@ class Solution(object):
             return 0
         maxLeftPathDown = max(self.maxPathDown(root.left), 0)
         maxRightPathDown = max(self.maxPathDown(root.right), 0)
+
+        # 在求maxPathDown的同时更新全局变量max_path_sum 剪枝的关键
         self.max_path_sum = max(maxLeftPathDown + maxRightPathDown + root.val, self.max_path_sum)
+
         return max(maxLeftPathDown, maxRightPathDown) + root.val
 
 
