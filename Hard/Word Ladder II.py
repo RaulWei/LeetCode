e37e00b783ef13e51bb886127b57a4e4cc98a314
Mingwei Wei <weimw0417@163.com>
2015-09-15 10:37:47 +0800

    AC Word Ladder II

--- Hard/Word Ladder II.py
+++ Hard/Word Ladder II.py
@@ -1,6 +1,13 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'wang'
 
+'''
+找出所有从start到end最短的路径
+改自Word Ladder 实际上还是BFS层序遍历 把图转换为树 一层一层遍历 触碰到end那么从start到end的路径即最短路径
+记录每个节点的父亲节点们 最后从end倒推回去 即可求出所有路径
+技巧：用set和defaultdict加速
+'''
+
 import collections
 
 class Solution(object):
@@ -15,27 +22,18 @@ class Solution(object):
             wordlist.remove(start)
         toVisit, dist = [], 1
         visited = set()
-        # level = dict()
-        # level[start] = 0
-        level = collections.defaultdict(int)
-        parents = collections.defaultdict(set)
+        level = collections.defaultdict(int)    # 记录节点所在的层 初始化为0
+        parents = collections.defaultdict(set)  # 记录节点的父亲节点们 初始化为空集合
         toVisit.append(start)   #
-        wordlist.append(end)
+        wordlist.add(end)
         while toVisit:
             num = len(toVisit)  # num记录该层的节点数
             for i in range(num):
                 word = toVisit.pop(0)
-                # if word == endWord:
-                #     # 遇到endWord可以返回
-                #     return dist
                 # 添加word节点的下一层
                 for i in range(len(word)):
-                    letter = word[i]
                     for c in 'abcdefghijklmnopqrstuvwxyz':
                         new_word = word[:i] + c + word[i + 1:]
-                        # if new_word == endWord:
-                        #     # 剪枝 虽然不在队头 但可以知道已经找到endWord 可以立即返回dist+1
-                        #     return dist + 1
                         if new_word in wordlist:    # 构造成功
                             if new_word not in visited: # 没访问过
                                 toVisit.append(new_word)
@@ -43,9 +41,9 @@ class Solution(object):
                                 level[new_word] = dist
                             if level[word] < level[new_word]:
                                 parents[new_word].add(word)
-                    # word = word[:i] + letter + word[i + 1:]
-            dist += 1
+            dist += 1   # 从start到此的dist+1 实际上也就是层数+1
 
+        # 由end向start构造路径
         res = [[end]]
         while res and res[0][0] != start:
             res = [[p] + r for r in res for p in parents[r[0]]]
@@ -54,5 +52,5 @@ class Solution(object):
 
 if __name__ == "__main__":
     sol = Solution()
-    sol.findLadders("a", "c", ["a", "b", "c"])
-    # sol.findLadders("hit", "cog", ["hot","dot","dog","lot","log"])
+    # sol.findLadders("a", "c", set(["a", "b", "c"]))
+    # sol.findLadders("hit", "cog", set(["hot","dot","dog","lot","log"]))
