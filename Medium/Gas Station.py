4b8c482796e81ecf92894b1b05da30b5e28ddee9
Mingwei Wei <weimw0417@163.com>
2015-09-15 16:15:37 +0800

    AC Gas Station

--- Medium/Gas Station.py
+++ Medium/Gas Station.py
@@ -6,17 +6,21 @@ class Solution(object):
     # :type cost: List[int]
     # :rtype: int
     def canCompleteCircuit(self, gas, cost):
-        lens = len(gas)
-        for start_index in range(lens):
+        lens, start_index = len(gas), 0
+        while start_index < lens:
             step, left_gas, find = 0, 0, True
             while step < lens:
-                left_gas += gas[step % lens] - cost[step % lens]
+                left_gas += gas[(start_index + step) % lens] - cost[(start_index + step) % lens]
                 if left_gas < 0:
                     find = False
+                    start_index += step % lens
+                    if start_index >= lens:
+                        return -1
                     break
                 step += 1
             if find is True:
                 return start_index
+            start_index += 1
         return -1
 
 
