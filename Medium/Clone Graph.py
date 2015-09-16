f25dd7c358a3b0af4b790005b6a4915a4a8c92c7
Mingwei Wei <weimw0417@163.com>
2015-09-16 10:36:16 +0800

    AC了 ....

--- Medium/Clone Graph.py
+++ Medium/Clone Graph.py
@@ -14,20 +14,21 @@ class Solution(object):
     def cloneGraph(self, node):
         if not node:
             return node
-        if node.label not in self.hasCopyed:
+        if node not in self.hasCopyed:
             new_node = UndirectedGraphNode(node.label)
-            self.hasCopyed[new_node.label] = new_node
+            self.hasCopyed[node] = new_node
             for n in node.neighbors:
-                self.hasCopyed[new_node.label].neighbors.append(self.cloneGraph(n))
-            return self.hasCopyed[new_node.label]
+                self.hasCopyed[node].neighbors.append(self.cloneGraph(n))
+            return self.hasCopyed[node]
         else:
-            return self.hasCopyed[node.label]
+            return self.hasCopyed[node]
 
 
 if __name__ == '__main__':
     p1 = UndirectedGraphNode(-1)
     p2 = UndirectedGraphNode(1)
-    p1.neighbors.append(p2)
+    p1.neighbors.append(p1)
+    p1.neighbors.append(p1)
     sol = Solution()
     p = sol.cloneGraph(p1)
     pass
