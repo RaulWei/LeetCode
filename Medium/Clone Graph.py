3e3d0de8189ce9959ffae75a1f6ea4a7eb104519
Mingwei Wei <weimw0417@163.com>
2015-09-16 11:26:24 +0800

    AC Clone Graph

--- Medium/Clone Graph.py
+++ Medium/Clone Graph.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'wang'
 
+'''
+神奇啊 我以node作为dict的index就神奇的AC了 但是我以node.label作为dict就不行（本地可以） 蛋疼啊
+'''
+
 # Definition for a undirected graph node
 class UndirectedGraphNode(object):
     def __init__(self, x):
@@ -16,12 +20,12 @@ class Solution(object):
             return node
         if node not in self.hasCopyed:
             new_node = UndirectedGraphNode(node.label)
-            self.hasCopyed[node] = new_node
+            self.hasCopyed[node] = new_node # 将node与new_node建立对应关系
             for n in node.neighbors:
                 self.hasCopyed[node].neighbors.append(self.cloneGraph(n))
-            return self.hasCopyed[node]
-        else:
-            return self.hasCopyed[node]
+        # node不在dict中 处理到此可返回 hasCopyed[node]
+        # node在dict中 直接返回 hasCopyed[node]
+        return self.hasCopyed[node]
 
 
 if __name__ == '__main__':
@@ -31,4 +35,3 @@ if __name__ == '__main__':
     p1.neighbors.append(p1)
     sol = Solution()
     p = sol.cloneGraph(p1)
-    pass
