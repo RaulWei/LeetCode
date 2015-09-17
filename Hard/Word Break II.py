a9283822ec22d9674d545711c140c57f2bbc9f7e
Mingwei Wei <weimw0417@163.com>
2015-09-17 11:12:22 +0800

    WA

--- Hard/Word Break II.py
+++ Hard/Word Break II.py
@@ -14,13 +14,21 @@ class Solution(object):
         if start == len(s):
             str = ' '.join(res)
             ret.append(str)
+            return True
+        for i in range(start, len(s) + 1):
+            if s[start: i] in wordDict:
+                break
+            elif i == len(s):
+                res = []
+                return False
         for i in range(start, len(s) + 1):
             if s[start: i] in wordDict:
                 res.append(s[start: i])
-                self.backtracking(ret, res, s, wordDict, i)
+                if not self.backtracking(ret, res, s, wordDict, i):
+                    return
                 res.pop()
-        return []
 
 if __name__ == '__main__':
     sol = Solution()
-    sol.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"])
+    # sol.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",set(["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
+    sol.wordBreak("catsanddog", set(["cat", "cats", "and", "sand", "dog"]))
