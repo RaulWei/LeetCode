67d35db20f7b6e332418b380cef9f4d275df7f2d
Mingwei Wei <weimw0417@163.com>
2015-09-17 11:48:10 +0800

    AC Word Break II

--- Hard/Word Break II.py
+++ Hard/Word Break II.py
@@ -15,26 +15,25 @@ class Solution(object):
             str = ' '.join(res)
             ret.append(str)
             return True
-        for i in range(start, len(s) + 1):
-            if s[start: i] in wordDict:
+        for i in range(start, len(s) + 1)[::-1]:
+            if s[i:] in wordDict:
                 break
-            elif i == len(s) and start != len(s):
+            elif i == start and start != len(s):
                 # res = []
                 return False
         for i in range(start, len(s) + 1):
             if s[start: i] in wordDict:
                 res.append(s[start: i])
-                if self.backtracking(ret, res, s, wordDict, i):
-                    res.pop()
-                else:
-                    return
-                # if not self.backtracking(ret, res, s, wordDict, i):
+                # if self.backtracking(ret, res, s, wordDict, i):
+                #     res.pop()
+                # else:
                 #     return
-                # res.pop()
+                self.backtracking(ret, res, s, wordDict, i)
+                res.pop()
         return True
 
 if __name__ == '__main__':
     sol = Solution()
     sol.wordBreak("aaaaaaa", set(["aaaa", "aaa"]))
-    # sol.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",set(["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
-    # sol.wordBreak("catsanddog", set(["cat", "cats", "and", "sand", "dog"]))
+    sol.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",set(["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]))
+    sol.wordBreak("catsanddog", set(["cat", "cats", "and", "sand", "dog"]))
