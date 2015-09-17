b3206a00085d1018452f220fab1642415b63c277
Mingwei Wei <weimw0417@163.com>
2015-09-17 17:06:56 +0800

    WA LRU Cache

---
+++ Hard/LRU Cache.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'wang'
+
+'''
+注意 这里的key-value对中的key并不是下标的意思
+'''
+
+class LRUCache(object):
+
+    # 成员变量
+    m_map = dict()
+    m_capacity = 0
+    m_cache = []
+
+    # :type capacity: int
+    def __init__(self, capacity):
+        self.m_capacity = capacity
+
+    # :rtype: int
+    def get(self, key):
+        if key not in self.m_map:
+            return -1
+        self.mov_to_head(key)
+        return self.m_map[key][1]
+        # return self.m_map[key].val
+
+    # :type key: int
+    # :type value: int
+    # :rtype: nothing
+    def set(self, key, value):
+        if key not in self.m_map:
+            # 不在map中 则插入新元素
+            self.m_cache.insert(0, [key, value])
+            self.m_map[key] = [key, value]
+            if len(self.m_cache) > self.m_capacity:
+                self.m_cache.pop()
+                self.m_map.pop(key)
+        else:
+            self.m_map[key][1] = value
+            self.mov_to_head(key)
+
+    def mov_to_head(self, key):
+        item = self.m_map[key]
+        self.m_cache.remove(item)
+        self.m_cache.insert(0, item)
+
+if __name__ == '__main__':
+    lru = LRUCache(1)
+    lru.set(2, 1)
+    print(lru.get(2))
