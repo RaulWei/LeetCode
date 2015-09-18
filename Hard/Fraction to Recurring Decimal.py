334b3f89a920c140ea95425862174a8c123a37b4
Mingwei Wei <weimw0417@163.com>
2015-09-18 15:27:25 +0800

    AC Fraction to Recurring Decimal

--- Hard/Fraction to Recurring Decimal.py
+++ Hard/Fraction to Recurring Decimal.py
@@ -1,18 +1,29 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'wang'
 
+'''
+模拟除法得到小数形式的结果
+注意几种情况：
+1 结果为0
+2 符号
+3 循环小数
+'''
+
 class Solution(object):
     # :type numerator: int
     # :type denominator: int
     # :rtype: str
     def fractionToDecimal(self, numerator, denominator):
-        res, last_numerator, flag = "", dict(), 0
+        # 初始化 判0 确定符号 统一处理
+        res, last_numerator = "", dict()
         if numerator == 0:
             return "0"
         if (numerator < 0 and denominator > 0) or (numerator > 0 and denominator < 0):
             res += '-'
         numerator = abs(numerator)
         denominator = abs(denominator)
+
+        # 循环处理
         while True:
             shang = numerator / denominator
             if not res or (len(res) == 1 and res[0] == '-'):
@@ -21,14 +32,16 @@ class Solution(object):
             else:
                 res += str(shang)
             numerator = (numerator - denominator * shang) * 10
-            if numerator in last_numerator:
+            if numerator in last_numerator: # 无限循环小数
                 loc = last_numerator[numerator]
                 res = res[:loc] + '(' + res[loc:] + ')'
                 break
-            if numerator == 0:
+            if numerator == 0:  # 非循环小数
                 if res[-1] == '.':
                     res = res[:-1]
                 break
+            # last_numerator记录上次被除数numerator出现的时候res到哪一位
+            # 如果numerator再次出现则说明小数有循环
             last_numerator[numerator] = len(res)
         return res
 
