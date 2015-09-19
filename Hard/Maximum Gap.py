e5df1a56ff13622f1eee3ce72ac8a89a6495182d
Mingwei Wei <weimw0417@163.com>
2015-09-19 12:00:10 +0800

    AC Maximum Gap

--- Hard/Maximum Gap.py
+++ Hard/Maximum Gap.py
@@ -16,17 +16,24 @@ class Solution(object):
         if N < 2:
             return 0
         min_n, max_n = min(nums), max(nums)
-        bk_len = int(math.ceil(float(max_n - min_n) / (N - 1)))   # ceil表示向上取整 bk_len表示每个区间长度
-        bk_num = N # bk_num表示区间个数
+        bk_len = max(1, int(math.ceil(float(max_n - min_n) / (N - 1))))   # ceil表示向上取整 bk_len表示每个区间长度
+        bk_num = (max_n - min_n) / bk_len + 1 # bk_num表示区间个数
         bks = [[] for i in range(bk_num)]    # 区间范围 左闭右开（最后一个区间双闭） [min, min+bk_len) [min+2*bk_len, min+3*bk_len) .. [min+n*bk_len, max]
         for n in nums:
             loc = (n - min_n) / bk_len
             bks[loc].append(n)
-        max_gap = 0
-        for i in range(1, len(bks)):
-            max_gap = max(max_gap, min(bks[i]) - max(bks[i - 1]))
+        max_gap, last_max = 0, 0
+        for i in range(len(bks)):
+            if i == 0:
+                if bks[i]:
+                    last_max = max(bks[i])
+                    continue
+            if bks[i]:
+                max_gap = max(max_gap, min(bks[i]) - last_max)
+                last_max = max(bks[i])
         return max_gap
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.maximumGap([1, 1, 1, 1]))
     print(sol.maximumGap([3, 2, 3]))
