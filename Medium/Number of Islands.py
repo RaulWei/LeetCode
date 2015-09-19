d1a09f30731cf1d1c0a99b3885460d3b6f26655d
Mingwei Wei <weimw0417@163.com>
2015-09-19 16:10:05 +0800

    update Number of Islands -- flag

--- Medium/Number of Islands.py
+++ Medium/Number of Islands.py
@@ -8,26 +8,26 @@ class Solution(object):
         if not grid:
             return 0
         row, col = len(grid), len(grid[0])
-        island_num = 2
+        island_num = 0
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == '1':
-                    self.BFS(i, j, grid, row, col, island_num)
+                    self.BFS(i, j, grid, row, col)
                     island_num += 1
-        return island_num - 2
+        return island_num
 
-    def BFS(self, row, col, grid, row_num, col_num, island_num):
+    def BFS(self, row, col, grid, row_num, col_num):
         direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 上 下 左 右
-        queue = []
-        queue.append([row, col])
+        queue = [[row, col]]
         while queue:
             cur = queue.pop(0)
             x, y = cur[0], cur[1]
+            grid[x] = "".join(grid[x][:y]) + 'T' + "".join(grid[x][y + 1:])
             for dire in direction:
                 new_x, new_y = x + dire[0], y + dire[1]
                 if 0 <= new_x < row_num and 0 <= new_y < col_num and grid[new_x][new_y] == '1':
                     queue.append([new_x, new_y])
-                    grid[new_x] = "".join(grid[new_x][:new_y]) + str(island_num) + "".join(grid[new_x][new_y + 1:])
+                    grid[new_x] = "".join(grid[new_x][:new_y]) + 'T' + "".join(grid[new_x][new_y + 1:])
 
 if __name__ == '__main__':
     sol = Solution()
