8010c2fd01ceca8d2a8e8174624fd107d21090cf
RaulWei <674877404@qq.com>
2015-09-20 21:38:48 +0800

    AC Number of Digit One

--- Medium/Number of Digit One.py
+++ Medium/Number of Digit One.py
@@ -1,6 +1,16 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+给定一个整数n，计算所有小于等于n的非负整数中数字1出现的次数
+http://bookshadow.com/weblog/2015/07/08/leetcode-number-digit-one/
+
+把数n看作   (high_n) cur (low_n), low_c = 10 ** cur位数
+cur = 0, high_n: 0 ~ high_n-1, low_n: 0 ~ low_c-1
+cur = 1, high_n: 0 ~ high_n-1, low_n: 0 ~ low_c-1 以及 high_n: high, low_n: 0 ~ low_n
+cur > 1, high_n: 0 ~ high_n, low_n: 0 ~ low_c-1
+'''
+
 class Solution:
     def countDigitOne(self, n):
         high_n, low_n, low_c = n, 0, 1
