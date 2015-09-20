c202ac5eac555daa56981bff9a09289526b0bcc3
RaulWei <674877404@qq.com>
2015-09-20 21:32:43 +0800

    AC Number of Digit One

--- Medium/Number of Digit One.py
+++ Medium/Number of Digit One.py
@@ -2,22 +2,24 @@
 __author__ = 'weimw'
 
 class Solution:
-    def containOne(self, n):
-        n_str = str(n)
-        count = 0
-        for i in range(len(n_str)):
-            if n_str[i] == '1':
-                count += 1
-        return count
-
-    # @param {integer} n
-    # @return {integer}
     def countDigitOne(self, n):
-        count = 0
-        for num in range(n+1):
-            count += self.containOne(num)
-        return count
+        high_n, low_n, low_c = n, 0, 1
+        one_num = 0
+        while high_n > 0:
+            cur = high_n % 10
+            high_n /= 10
+            if cur == 0:
+                one_num += high_n * low_c
+            elif cur == 1:
+                one_num += high_n * low_c
+                one_num += low_n + 1
+            else:
+                one_num += (high_n + 1) * low_c
+            low_c *= 10
+            low_n = n - high_n * low_c
+        return one_num
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.countDigitOne(11))
     print(sol.countDigitOne(13))
