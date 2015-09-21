0d968615dd02651ec2737aeb23b830cb3364003f
RaulWei <674877404@qq.com>
2015-09-21 23:11:33 +0800

    AC Count Primes

--- Easy/Count Primes.py
+++ Easy/Count Primes.py
@@ -7,20 +7,28 @@ class Solution(object):
     # :type n: int
     # :rtype: int
     def countPrimes(self, n):
+        if n < 2:
+            return 0
         isPrime = [True for i in range(n)]
-        for i in range(2, int(math.sqrt(n))):
+        count = n - 2
+        for i in range(2, int(math.sqrt(n)) + 1):
             if not isPrime[i]:
                 continue
             j = i ** 2
             while j < n:
-                isPrime[j] = False
+                if isPrime[j]:
+                    isPrime[j] = False
+                    count -= 1
                 j += i
-        ret = []
-        for i in range(2, n):
-            if isPrime[i]:
-                ret.append(i)
-        return ret
+        return count
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.countPrimes(0))
+    print(sol.countPrimes(1))
+    print(sol.countPrimes(2))
+    print(sol.countPrimes(3))
+    print(sol.countPrimes(5))
     print(sol.countPrimes(13))
+    print(sol.countPrimes(120))
+    print(sol.countPrimes(1500000))
