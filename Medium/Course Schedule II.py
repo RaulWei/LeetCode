04c0e3e3bd3cab2b020d75614b1bf03095a52273
Mingwei Wei <weimw0417@163.com>
2015-09-21 20:32:46 +0800

    update Course Schedule II

---
+++ Medium/Course Schedule II.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'wang'
+
+class Solution(object):
+    # :type numCourses: int
+    # :type prerequisites: List[List[int]]
+    # :rtype: bool
+    def findOrder(self, numCourses, prerequisites):
+        ret = []
+        in_degree, next = dict(), dict()    # 构建入度字典 key为节点值 value为入度; 构建next字典 key为当前节点 value连接的节点们
+        for edge in prerequisites:
+            if edge[0] not in in_degree:
+                in_degree[edge[0]] = 0
+            if edge[1] not in in_degree:
+                in_degree[edge[1]] = 1
+            else:
+                in_degree[edge[1]] += 1
+            if edge[0] not in next:
+                next[edge[0]] = [edge[1]]
+            else:
+                next[edge[0]].append(edge[1])
+        while in_degree:
+            # 找到入度为0的节点 POP 继续找
+            # 如果找不到入度为0的节点 说明存在有向图的环 返回False
+            find = False
+            for key in in_degree:
+                if in_degree[key] == 0:
+                    if key in next:
+                        for nx in next[key]:
+                            in_degree[nx] -= 1
+                    ret.append(key)
+                    in_degree.pop(key)
+                    find = True
+                    break
+            if not find:
+                return []
+        return ret[::-1]
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
