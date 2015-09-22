2ae5b9217a7257fdee24fb0301abc4b15ef3ec40
Mingwei Wei <weimw0417@163.com>
2015-09-22 18:18:36 +0800

    AC Best Time to Buy and Sell Stock III

--- Hard/Best Time to Buy and Sell Stock III.py
+++ Hard/Best Time to Buy and Sell Stock III.py
@@ -2,15 +2,14 @@
 __author__ = 'wang'
 
 '''
-f[k, i]表示从prices[0]到prices[i]经过k次transactions所能够获得的最大收益
+f[k, i]表示从prices[0]到prices[i]最多经过k次transactions所能够获得的最大收益
 初态：f[0, i] = 0 （0次transaction当然收益为0） f[k, 0] = 0 （prices只有一个 实现不了买入卖出操作 至少要两个节点）
 终态：max(f[k, i])
 递推公式：
-f[k, i] = max(f[k, i - 1], prices[i] - prices[j] + f[k - 1, j]) 0 <= j < i
-        = max(f[k, i - 1], prices[i] + max(f[k - 1, j] - prices[j]) (0 <= j < i) ))
+f[k, i] = max(在i之前就已经得到最大利润, i处卖出 j处买入所获得的利润 + f[k - 1, j - 1])
+        = max(f[k, i - 1], prices[i] - prices[j] + f[k - 1, j - 1]) 0 <= j < i
+        = max(f[k, i - 1], prices[i] + max(f[k - 1, j - 1] - prices[j]) (0 <= j < i) ))
 
-为什么是 prices[i] - prices[j] + f[k-1,j]
-这里的f[k-1,j]中的j我们如果不取 那么就是0-j-1 j-i两段； 如果取 那么其实就是0-i中取某一段 也是符合的
 j的取值是[0,i) 没必要再来个循环求max(f[k - 1, j] - prices[j]) 这样会TLE 直接合在i的这层循环中做 两重循环不超时
 '''
 
@@ -21,7 +20,7 @@ class Solution(object):
         if not prices:
             return 0
 
-        K, max_prof = 2, 0
+        K = 2
         f = [[0 for col in range(len(prices))] for row in range(K + 1)]
 
         # 递推
@@ -29,13 +28,13 @@ class Solution(object):
             max_tmp = f[k - 1][0] - prices[0]
             for i in range(1, len(prices)):
                 f[k][i] = max(f[k][i - 1], prices[i] + max_tmp)
-                max_tmp = max(max_tmp, f[k - 1][i] - prices[i])
-                max_prof = max(max_prof, f[k][i])
+                max_tmp = max(max_tmp, f[k - 1][i - 1] - prices[i])
 
-        return max_prof
+        return f[K][len(prices) - 1]
 
 if __name__ == '__main__':
     sol = Solution()
     print(sol.maxProfit([]))
+    print(sol.maxProfit([1, 2, 3]))
     print(sol.maxProfit([1, 1, 2, 1, 1]))
     print(sol.maxProfit([1, 2, 3, 1, 2, 4]))
