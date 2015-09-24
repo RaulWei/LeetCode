566813492340f52ed038f076ed13de6ddfb830b8
Mingwei Wei <weimw0417@163.com>
2015-09-24 12:56:51 +0800

    update Word Search II add trie

--- Hard/Word Search II.py
+++ Hard/Word Search II.py
@@ -1,6 +1,79 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'wang'
 
+class TrieNode:
+    # Initialize your data structure here.
+    def __init__(self, char=''):
+        self.son = []
+        self.isWord = False
+        self.char = char
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    # @param {string} word
+    # @return {void}
+    # Inserts a word into the trie.
+    def insert(self, word):
+        root = self.root
+        for i in range(len(word)):
+            find = False
+            for son in root.son:
+                if word[i] == son.char:
+                    # 在root的son中找到word的某个字符
+                    root = son
+                    find = True
+                    break
+            if find is False:
+                # w不存在于root的son中
+                root.son.append(TrieNode(word[i]))
+                root = root.son[-1]
+            if i == len(word) - 1:
+                root.isWord = True
+        return
+
+    # @param {string} word
+    # @return {boolean}
+    # Returns if the word is in the trie.
+    def search(self, word):
+        root = self.root
+        for i in range(len(word)):
+            find = False
+            for son in root.son:
+                if word[i] == son.char:
+                    # 找到word[i]
+                    root = son
+                    find = True
+                    if i == len(word) - 1 and root.isWord is True:
+                        return True
+                    break
+            if find is False:
+                # word[i]没找到 可以直接返回False
+                return False
+        # 例如insert('ab'), search('a')
+        return False
+
+    # @param {string} prefix
+    # @return {boolean}
+    # Returns if there is any word in the trie
+    # that starts with the given prefix.
+    def startsWith(self, prefix):
+        root = self.root
+        for i in range(len(prefix)):
+            find = False
+            for son in root.son:
+                if prefix[i] == son.char:
+                    root = son
+                    find = True
+                    if i == len(prefix) - 1:
+                        return True
+                    break
+            if find is False:
+                # prefix[i]没找到可以直接返回False
+                return False
+
 class Solution(object):
     # :type board: List[List[str]]
     # :type words: List[str]
@@ -41,5 +114,6 @@ class Solution(object):
         return False
 
 if __name__ == '__main__':
+    trie = Trie()
     sol = Solution()
     print(sol.findWords(["oaan", "etae", "ihkr", "iflv"], ["eat", "oath"]))
