9db3aa331831014610e4cd0c2362c948c80f69f0
Mingwei Wei <weimw0417@163.com>
2015-09-25 13:54:41 +0800

    WA H-Index

--- Medium/H-Index II.py
+++ Medium/H-Index II.py
@@ -1,14 +1,27 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'wang'
 
+'''
+二分法加速
+'''
+
 class Solution(object):
     # :type citations: List[int]
     # :rtype: int
     def hIndex(self, citations):
         length = len(citations)
-        for i in range(length)[::-1]:
-            if length - i >= citations[i]:
-                return citations[i]
+        # left, right = 0, length - 1
+        # while left < right:
+        #     mid = (left + right) / 2
+        for i in range(length):
+            if length - i < citations[i]:
+                return i + 1
+            j = length - 1 - i
+            if length - j >= citations[j]:
+                return citations[j]
         return 0
 
-
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.hIndex([100]))
+    print(sol.hIndex([0, 1, 3, 5, 6]))
