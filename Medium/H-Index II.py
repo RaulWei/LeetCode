be52ab8f04004515c268df4ed513a2d1701da486
Mingwei Wei <weimw0417@163.com>
2015-09-25 14:34:28 +0800

    AC H-Index II

--- Medium/H-Index II.py
+++ Medium/H-Index II.py
@@ -2,7 +2,10 @@
 __author__ = 'wang'
 
 '''
-二分法加速
+给定是一段排好序的数组 所以有了二分的前提
+采用二分法加速
+找到index 如果citations[index] >= length - index 那么这个index就是其中一种合法的index return的是length - index
+但我们要找的是 max(length - index) 所以还要往左找
 '''
 
 class Solution(object):
@@ -10,15 +13,16 @@ class Solution(object):
     # :rtype: int
     def hIndex(self, citations):
         if not citations or citations[-1] == 0:
+            # 特例处理
             return 0
         length = len(citations)
         left, right = 0, length - 1
         while left < right:
             mid = (left + right) / 2
             if length - mid <= citations[mid]:
-                right = mid
+                right = mid # 这个mid是合法的index 用right记录合法的index
             else:
-                left = mid + 1
+                left = mid + 1  # 这个mid不合法 没必要为它浪费时间 所以令left为mid的下一个
         return length - right
 
 if __name__ == '__main__':
