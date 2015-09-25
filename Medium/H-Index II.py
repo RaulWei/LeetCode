68fb280e7c8a735d23e4ee2d6c2cbe590085ef18
Mingwei Wei <weimw0417@163.com>
2015-09-25 14:22:48 +0800

    AC H-Index II

--- Medium/H-Index II.py
+++ Medium/H-Index II.py
@@ -9,19 +9,22 @@ class Solution(object):
     # :type citations: List[int]
     # :rtype: int
     def hIndex(self, citations):
+        if not citations or citations[-1] == 0:
+            return 0
         length = len(citations)
-        # left, right = 0, length - 1
-        # while left < right:
-        #     mid = (left + right) / 2
-        for i in range(length):
-            if length - i < citations[i]:
-                return i + 1
-            j = length - 1 - i
-            if length - j >= citations[j]:
-                return citations[j]
-        return 0
+        left, right = 0, length - 1
+        while left < right:
+            mid = (left + right) / 2
+            if length - mid <= citations[mid]:
+                right = mid
+            else:
+                left = mid + 1
+        return length - right
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.hIndex([11, 15]))
+    print(sol.hIndex([0, 0, 1]))
     print(sol.hIndex([100]))
+    print(sol.hIndex([1, 2, 3, 4, 5, 6]))
     print(sol.hIndex([0, 1, 3, 5, 6]))
