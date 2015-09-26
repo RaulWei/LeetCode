13e7b9622f8a27d8a99ed757251dc9554669f973
RaulWei <674877404@qq.com>
2015-09-26 19:32:31 +0800

    AC Perfect Squares

--- Medium/Perfect Squares.py
+++ Medium/Perfect Squares.py
@@ -1,8 +1,6 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
-import math
-
 '''
 f(n)代表numSquares
 初态：f(0) = 0, f(1) = 1
@@ -12,16 +10,21 @@ f(n) = min(f(i) + 1 if n - i is perfect square number) 1 <= i < n
 '''
 
 class Solution(object):
+
+    f = [0, 1]
+
     # :type n: int
     # :rtype: int
     def numSquares(self, n):
-        f = [i for i in range(n + 1)]
-        for i in range(2, n + 1):
+        lenf = len(self.f)
+        while lenf <= n:
+            self.f.append(lenf)
             j = 1
-            while j * j <= i:
-                f[i] = min(f[i], f[i - j * j] + 1)
+            while j * j <= lenf:
+                self.f[lenf] = min(self.f[lenf], self.f[lenf - j * j] + 1)
                 j += 1
-        return f[n]
+            lenf += 1
+        return self.f[n]
 
 
 if __name__ == '__main__':
@@ -30,4 +33,7 @@ if __name__ == '__main__':
     print(sol.numSquares(13))
     print(sol.numSquares(254))
     print(sol.numSquares(266))
+    print(sol.numSquares(10000))
+    print(sol.numSquares(4635))
     print(sol.numSquares(9975))
+    print(sol.numSquares(7691))
