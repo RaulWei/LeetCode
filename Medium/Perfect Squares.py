05692bd88be0d4f737ee117d87d3f7c08b86991e
RaulWei <674877404@qq.com>
2015-09-26 11:32:56 +0800

    update Perfect Squares

---
+++ Medium/Perfect Squares.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+'''
+f(n)代表numSquares
+初态：
+终态：
+递推公式：
+
+'''
+
+class Solution(object):
+    # :type n: int
+    # :rtype: int
+    def numSquares(self, n):
