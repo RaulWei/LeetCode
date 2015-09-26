068680f43abb3427d63f99833d19d482f434eb4f
RaulWei <674877404@qq.com>
2015-09-26 10:54:25 +0800

    AC First Bad Version

--- Easy/First Bad Version.py
+++ Easy/First Bad Version.py
@@ -1,6 +1,12 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+给定的n代表有[1, 2, .. n]这么多个版本号 其中某个版本坏了 导致其之后的版本全都坏了
+所以要快速找到第一个坏了的版本号
+显然 二分法
+'''
+
 # The isBadVersion API is already defined for you.
 # @param version, an integer
 # @return a bool
