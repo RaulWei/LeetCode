9fc508ad1f5a4294cea1b34766fdaf51aa012b06
RaulWei <674877404@qq.com>
2015-09-27 20:40:09 +0800

    WA Integer to English Words

--- Medium/Integer to English Words.py
+++ Medium/Integer to English Words.py
@@ -17,13 +17,25 @@ class Solution(object):
         "", "Ten", "Twenty", "Thirty", "Forty", "Fifty",
         "Sixty", "Seventy", "Eighty", "Ninety"
     ]
+    table_level = [
+        "", "Thousand", "Million", "Billion"
+    ]
 
     # :type num: int
     # :rtype: str
     def numberToWords(self, num):
         if num == 0:
             return "Zero"
-        return self.numberToWords_less1000(num)
+        ret, i = [], 0
+        while num > 0:
+            num_tmp = num % 1000
+            num /= 1000
+            if i > 0:
+                ret.append(self.numberToWords_less1000(num_tmp) + " " + self.table_level[i])
+            else:
+                ret.append(self.numberToWords_less1000(num_tmp))
+            i += 1
+        return ' '.join(ret[::-1])
 
     def numberToWords_less1000(self, num):
         res = []
@@ -35,17 +47,20 @@ class Solution(object):
             res.append("Hundred")
         # 十位和个位
         if num <= 20:
-            res.append(self.table_less20[num])
+            if num  > 0:
+                res.append(self.table_less20[num])
         else:
             res.append(self.table_decade[num / 10])
-            res.append(self.table_less20[num % 10])
+            if num % 10 > 0:
+                res.append(self.table_less20[num % 10])
         return ' '.join(res)
 
 if __name__ == '__main__':
     sol = Solution()
+    print(sol.numberToWords(30))
+    print(sol.numberToWords(123))
     print(sol.numberToWords(0))
     print(sol.numberToWords(22))
-    print(sol.numberToWords_less1000(200))
-    print(sol.numberToWords_less1000(10))
-    print(sol.numberToWords_less1000(998))
+    print(sol.numberToWords(12345))
+    print(sol.numberToWords(1234567891))
 
