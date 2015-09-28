be12f88f45be4c3dddbd1643d27a55074b6dc4a1
Mingwei Wei <weimw0417@163.com>
2015-09-28 11:01:32 +0800

    update Single Number III

--- Medium/Single Number III.py
+++ Medium/Single Number III.py
@@ -5,4 +5,12 @@ class Solution(object):
     # :type nums: List[int]
     # :rtype: List[int]
     def singleNumber(self, nums):
+        # 先拿到a和b的异或
+        axorb = 0
+        for i in range(nums):
+            axorb ^= i
+        pass
 
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.singleNumber([1, 2, 1, 3, 2, 5]))
