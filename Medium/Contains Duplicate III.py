3461738036c32c98e7ab220fcd62428e934de001
RaulWei <674877404@qq.com>
2015-09-29 21:01:09 +0800

    update Contains Duplicate III

--- Medium/Contains Duplicate III.py
+++ Medium/Contains Duplicate III.py
@@ -1,18 +1,20 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+这题自己想不出来 看了题解自己还想了好久
+其实主要用到一个bucket 只不过bucket的宽度设宽了 取t + 1
+bucket, key: (num[i] - MIN_VALUE) / (t + 1), value: num[i] - MIN_VALUE
+引入MIN_VALUE是用来兼容num中出现负数的情况
+'''
+
 class Solution(object):
     # :type nums: List[int]
     # :type k: int
     # :type t: int
     # :rtype: bool
     def containsNearbyAlmostDuplicate(self, nums, k, t):
-        if len(nums) >= 2:
-            for i in range(len(nums)):
-                for j in range(i + 1, min(i + k + 1, len(nums))):
-                    if abs(nums[j] - nums[i]) <= t:
-                        return True
-        return False
+        pass
 
 if __name__ == '__main__':
     sol = Solution()
