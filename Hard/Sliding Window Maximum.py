f0d2eb370f2b68b24faa859e5b775958583380be
RaulWei <674877404@qq.com>
2015-10-01 17:19:03 +0800

    add Sliding Window Maximum

---
+++ Hard/Sliding Window Maximum.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+'''
+建立一个队列deque 存num的下标
+建立一个数组ret 存返回结果
+'''
+
+class Solution(object):
+    # :type nums: List[int]
+    # :type k: int
+    # :rtype: List[int]
+    def maxSlidingWindow(self, nums, k):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
