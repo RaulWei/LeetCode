8039dc8d2dbd1327013526c9e2da186c0471b65e
RaulWei <674877404@qq.com>
2015-10-01 19:23:21 +0800

    AC Sliding Window Maximum

--- Hard/Sliding Window Maximum.py
+++ Hard/Sliding Window Maximum.py
@@ -4,6 +4,7 @@ __author__ = 'weimw'
 '''
 建立一个队列deque 存num的下标
 建立一个数组ret 存返回结果
+queue[0]存当前window中的最大值的下标
 '''
 
 class Solution(object):
@@ -14,12 +15,15 @@ class Solution(object):
         queue, ret = [], []
         for i in range(len(nums)):
             while queue and queue[0] < i - k + 1:
+                # 超过window的size 则删除
                 queue.pop(0)
             while queue and nums[queue[-1]] < nums[i]:
+                # 从右向左把比当前的nums[i]小的都删掉
                 queue.pop()
+            # 经过前面两步的处理 在这里把当前数添加进队列
             queue.append(i)
             if i >= k - 1:
-                ret.append(nums[queue[0]])
+                ret.append(nums[queue[0]])  # queue[0]存的是当前window中最大值的下标
         return ret
 
 if __name__ == '__main__':
