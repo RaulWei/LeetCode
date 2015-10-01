ef0a505fa0bb43bc8205a179d9f9e8b4f85a5e6c
RaulWei <674877404@qq.com>
2015-10-02 00:01:24 +0800

    AC The Skyline Problem

--- Hard/The Skyline Problem.py
+++ Hard/The Skyline Problem.py
@@ -4,6 +4,11 @@ __author__ = 'weimw'
 '''
 http://segmentfault.com/a/1190000003786782
 http://www.cnblogs.com/easonliu/p/4531020.html
+感觉这题算是很难的题了 ... 也是自己没有想法 ... 看题解还要想很久的题目
+还是很神奇的 .. 不知道他们怎么想出来的算法
+
+如果不维持一个有序的heap 那么每次取cur为当前最高高度时 cur = max(heap) 无疑TLE
+那么我用二分法优化了插入排序 维持heap为一个有序的数组 可以AC
 '''
 
 class Solution(object):
@@ -12,15 +17,19 @@ class Solution(object):
     def getSkyline(self, buildings):
         height = []
         for b in buildings:
-            height.append([b[0], -b[2]])
-            height.append([b[1], b[2]])
-        height = sorted(height)
+            # 把水平线拆成左顶点和右顶点
+            height.append([b[0], -b[2]])    # 左顶点的高用负数表示 来区分出它是左边顶点
+            height.append([b[1], b[2]])     # 右顶点的高为正数
+        height = sorted(height)     # 按x轴坐标排序
         heap, res = [0], []
-        pre, cur = 0, 0
-        for h in height:
+        pre, cur = 0, 0     # pre表示上一轮最高度 cur表示当前最高度
+        for h in height:    # 按x轴从左到右遍历
             if h[1] < 0:
+                # 左顶点
+                # 保证heap是一个升序的数组 heap的最后一个元素是当前维持的最高building值
                 heap.insert(self.getInsertPos(heap, -h[1]), -h[1])
             else:
+                # 遇到右顶点 把左顶点的高从heap消灭
                 heap.remove(h[1])
             cur = heap[-1]
             if cur != pre:
