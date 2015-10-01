47f979343b0f211d83da1bcca28e56419ff7fa6e
RaulWei <674877404@qq.com>
2015-10-01 23:27:21 +0800

    TLE The Skyline Problem

--- Hard/The Skyline Problem.py
+++ Hard/The Skyline Problem.py
@@ -20,7 +20,8 @@ class Solution(object):
         pre, cur = 0, 0
         for h in height:
             if h[1] < 0:
-                heap.append(-h[1])
+                # heap.append(-h[1])
+                heap.insert(self.getInsertPos(buildings, -h[1]), -h[1])
             else:
                 heap.remove(h[1])
             cur = max(heap)
@@ -29,6 +30,19 @@ class Solution(object):
                 pre = cur
         return res
 
+    def getInsertPos(self, nums, ins):
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] < ins:     # mid这个位置肯定不可取
+                left = mid + 1
+            elif nums[mid] > ins:
+                right = mid - 1
+            else:
+                return mid
+        return left
+
 if __name__ == '__main__':
     sol = Solution()
+    # print(sol.getInsertPos([1,2 ,3,7],7))
     print(sol.getSkyline([[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]))
