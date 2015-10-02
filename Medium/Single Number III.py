f9bc1cb8f9add8dd21f5f2161c9b707149abc488
RaulWei <674877404@qq.com>
2015-10-02 22:21:28 +0800

    AC Single Number III

--- Medium/Single Number III.py
+++ Medium/Single Number III.py
@@ -5,11 +5,20 @@ class Solution(object):
     # :type nums: List[int]
     # :rtype: List[int]
     def singleNumber(self, nums):
-        # 先拿到a和b的异或
+        res = [0, 0]
+        # 先求出a和b的异或
         axorb = 0
-        for i in range(nums):
-            axorb ^= i
-        pass
+        for n in nums:
+            axorb ^= n
+        # a和b相异 因此axorb中必有至少一位为1
+        diff = axorb
+        diff &= -diff
+        for n in nums:
+            if n & diff == 0:
+                res[0] ^= n
+            else:
+                res[1] ^= n
+        return res
 
 if __name__ == '__main__':
     sol = Solution()
