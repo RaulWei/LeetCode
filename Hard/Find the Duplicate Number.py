01687d7897b44d6aeae85bab65fbd091959b3da2
RaulWei <674877404@qq.com>
2015-10-04 11:25:23 +0800

    AC Find the Duplicate Number

--- Hard/Find the Duplicate Number.py
+++ Hard/Find the Duplicate Number.py
@@ -1,20 +1,26 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+鸽巢原理
+给定n + 1个元素 每个元素的范围是[1, n] 肯定有重复的元素 要求找出这个元素
+'''
+
 class Solution(object):
     # :type nums: List[int]
     # :rtype: int
     def findDuplicate(self, nums):
         low, high = 1, len(nums) - 1
         while low < high:
-            mid =(low + high) / 2
+            mid = (low + high) / 2
             count = 0
             for n in nums:
-                count += 1 if n <= mid else 0
-            if count > mid:
-                # [1, mid]之间有>mid个数 那么鸽巢原理可知其中必有重复
+                count += 1 if low <= n <= mid else 0
+            if count > mid - low + 1:
+                # [low, mid]之间有 > mid - low + 1个数 那么鸽巢原理可知其中必有重复
                 high = mid
             else:
+                # [mid + 1, high]之中必有重复
                 low = mid + 1
         return low
 
