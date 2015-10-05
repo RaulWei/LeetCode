24a440179c16c1eec88050c9f5fb52ca58c48bc7
RaulWei <674877404@qq.com>
2015-10-05 11:17:36 +0800

    AC Game of Life

--- Medium/Game of Life.py
+++ Medium/Game of Life.py
@@ -9,12 +9,14 @@ class Solution(object):
         direction = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
         for i in range(row):
             for j in range(col):
-                count = 0
+                count = 0   # 统计board[i][j]周围的live个数
                 for dire in direction:
                     new_i, new_j = i + dire[0], j + dire[1]
                     if 0 <= new_i < row and 0 <= new_j < col and board[new_i][new_j] & 1 == 1:
                         count += 1
-                if count == 3 or count == 2:
+                if board[i][j] & 1 == 1 and (count == 3 or count == 2):
+                    board[i][j] |= 2
+                if board[i][j] & 1 == 0 and count == 3:
                     board[i][j] |= 2
         for i in range(row):
             for j in range(col):
