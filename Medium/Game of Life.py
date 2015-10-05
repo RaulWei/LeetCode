bae8699f6f6520a848ecb0028f48207270b67f4d
RaulWei <674877404@qq.com>
2015-10-05 11:10:44 +0800

    WA Game of Life

---
+++ Medium/Game of Life.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution(object):
+    # :type board: List[List[int]]
+    # :rtype: void Do not return anything, modify board in-place instead.
+    def gameOfLife(self, board):
+        row, col = len(board), len(board[0])
+        direction = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+        for i in range(row):
+            for j in range(col):
+                count = 0
+                for dire in direction:
+                    new_i, new_j = i + dire[0], j + dire[1]
+                    if 0 <= new_i < row and 0 <= new_j < col and board[new_i][new_j] & 1 == 1:
+                        count += 1
+                if count == 3 or count == 2:
+                    board[i][j] |= 2
+        for i in range(row):
+            for j in range(col):
+                board[i][j] >>= 1
