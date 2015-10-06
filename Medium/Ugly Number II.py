f97cdfc824e4fc258c7104098cd40b350f2ae793
RaulWei <674877404@qq.com>
2015-10-06 23:28:16 +0800

    AC Ugly Number II

--- Medium/Ugly Number II.py
+++ Medium/Ugly Number II.py
@@ -1,6 +1,18 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+掌握规律就不难 没掌握规律就很难
+k[0] = 1
+k[1] = min(k[0] * 2, k[0] * 3, k[0] * 5) = min(2, 3, 5) = 2
+k[2] = min(k[1] * 2, k[0] * 3, k[0] * 5) = min(4, 3, 5) = 3
+k[3] = min(k[1] * 2, k[1] * 3, k[0] * 5) = min(4, 6, 5) = 4
+k[4] = min(k[2] * 2, k[1] * 3, k[0] * 5) = min(6, 6, 5) = 5
+k[5] = min(k[2] * 2, k[1] * 3, k[1] * 5) = min(6, 6, 10) = 6
+k[6] = min(k[3] * 2, k[2] * 3, k[1] * 5) = min(8, 9, 10) = 8
+...
+'''
+
 class Solution(object):
     # :type n: int
     # :rtype: int
@@ -18,5 +30,3 @@ class Solution(object):
 if __name__ == '__main__':
     sol = Solution()
     print(sol.nthUglyNumber(4))
-
-
