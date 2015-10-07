9d3c3742116006c7fe1c0710f728ed66e04ced1e
RaulWei <674877404@qq.com>
2015-10-07 14:30:56 +0800

    add Expression Add Operators

---
+++ Hard/Expression Add Operators.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'weimw'
+
+class Solution(object):
+    # :type num: str
+    # :type target: int
+    # :rtype: List[str]
+    def addOperators(self, num, target):
+        pass
+
+    def DFS(self, ):
+        pass
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.addOperators("123", 6))
+    print(sol.addOperators("232", 8))
+    print(sol.addOperators("105", 5))
+    print(sol.addOperators("00", 0))
+    print(sol.addOperators("3456237490", 9191))
