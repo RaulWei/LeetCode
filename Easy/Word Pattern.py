f808b808f346249a0fd8e345bb095f2d5491c7fc
RaulWei <674877404@qq.com>
2015-10-07 12:09:59 +0800

    AC Word Pattern

--- Easy/Word Pattern.py
+++ Easy/Word Pattern.py
@@ -1,6 +1,10 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'weimw'
 
+'''
+pattern与str中的word必须一一对应 所以我这里开两个dict
+'''
+
 class Solution(object):
     # :type pattern: str
     # :type str: str
@@ -8,12 +12,12 @@ class Solution(object):
     def wordPattern(self, pattern, str):
         dic1, dic2 = dict(), dict()
         stri = str.split()
-        if len(stri) != len(pattern):
+        if len(stri) != len(pattern):   # 长度不一致 直接False
             return False
         for i in range(len(pattern)):
-            if pattern[i] in dic1 and dic1[pattern[i]] != stri[i]:
+            if pattern[i] in dic1 and dic1[pattern[i]] != stri[i]:  # pattern对应stri
                 return False
-            if stri[i] in dic2 and dic2[stri[i]] != pattern[i]:
+            if stri[i] in dic2 and dic2[stri[i]] != pattern[i]: # stri对应pattern
                 return False
             dic1[pattern[i]] = stri[i]
             dic2[stri[i]] = pattern[i]
